c05f9f343fbf829571b301d4bd092d6ef91f9763
Rohan Jagtap <rohanj30@icloud.com>
2022-08-21 01:38:51 +0530

    Time: 322 ms (50.25%), Space: 15.2 MB (97.00%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.py
+++ 941-valid-mountain-array/941-valid-mountain-array.py
@@ -5,15 +5,26 @@ class Solution:
         if length < 3:
             return False
         
-        sign = 1
-        for i in range(1, length):
-            if arr[i] - arr[i - 1] == 0:
-                return False
-            elif arr[i] - arr[i - 1] < 0:
-                if i == 1:
-                    return False
-                sign = -1
-            elif sign == -1:
-                    return False
+        # sign = 1
+        # for i in range(1, length):
+        #     if arr[i] - arr[i - 1] == 0:
+        #         return False
+        #     elif arr[i] - arr[i - 1] < 0:
+        #         if i == 1:
+        #             return False
+        #         sign = -1
+        #     elif sign == -1:
+        #             return False
+        
+        i = 0
+        
+        while i < (length - 1) and arr[i] < arr[i + 1]:
+            i += 1
+            
+        if i == 0 or i == (length - 1):
+            return False
+        
+        while i < (length - 1) and arr[i] > arr[i + 1]:
+            i += 1
                 
-        return sign == -1
+        return i == (length - 1)
