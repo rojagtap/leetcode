ab29c5e22f0047cd07dfe16e70f830a60937875d
Rohan Jagtap <rohanj30@icloud.com>
2022-08-21 00:14:06 +0530

    Time: 418 ms (18.86%), Space: 14.7 MB (49.78%) - LeetHub

---
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -0,0 +1,26 @@
+class OrderedStream:
+
+    def __init__(self, n: int):
+        self.n = n
+        self.stream = [None] * self.n
+        self.ptr = 0
+        
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.stream[idKey - 1] = value
+        if (idKey - 1) == self.ptr:
+            start = self.ptr
+            while self.ptr < self.n and self.stream[self.ptr] != None:
+                self.ptr += 1
+            else:
+                if self.ptr == (self.n - 1) and self.stream[self.ptr] != None:
+                    self.ptr += 1
+            
+            return self.stream[start: self.ptr]
+        else:
+            return []
+
+
+# Your OrderedStream object will be instantiated and called as such:
+# obj = OrderedStream(n)
+# param_1 = obj.insert(idKey,value)
