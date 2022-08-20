bfd6c268eff5c89eb3390a7cdcf7431537fdfc0b
Rohan Jagtap <rohanj30@icloud.com>
2022-08-21 01:41:49 +0530

    Time: 423 ms (12.15%), Space: 15.5 MB (22.57%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.py
+++ 941-valid-mountain-array/941-valid-mountain-array.py
@@ -1,39 +1,39 @@
 class Solution:
-    def validMountainArray(self, arr: List[int]) -> bool:
-        length = len(arr)
-        
-        if length < 3:
-            return False
-                
-        i = 0
-        
-        while i < (length - 1) and arr[i] < arr[i + 1]:
-            i += 1
-            
-        if i == 0 or i == (length - 1):
-            return False
-        
-        while i < (length - 1) and arr[i] > arr[i + 1]:
-            i += 1
-                
-        return i == (length - 1)
-    
-    
 #     def validMountainArray(self, arr: List[int]) -> bool:
 #         length = len(arr)
         
 #         if length < 3:
 #             return False
-        
-#         sign = 1
-#         for i in range(1, length):
-#             if arr[i] - arr[i - 1] == 0:
-#                 return False
-#             elif arr[i] - arr[i - 1] < 0:
-#                 if i == 1:
-#                     return False
-#                 sign = -1
-#             elif sign == -1:
-#                     return False
                 
-#         return sign == -1
+#         i = 0
+        
+#         while i < (length - 1) and arr[i] < arr[i + 1]:
+#             i += 1
+            
+#         if i == 0 or i == (length - 1):
+#             return False
+        
+#         while i < (length - 1) and arr[i] > arr[i + 1]:
+#             i += 1
+                
+#         return i == (length - 1)
+    
+    
+    def validMountainArray(self, arr: List[int]) -> bool:
+        length = len(arr)
+        
+        if length < 3:
+            return False
+        
+        sign = 1
+        for i in range(1, length):
+            if arr[i] - arr[i - 1] == 0:
+                return False
+            elif arr[i] - arr[i - 1] < 0:
+                if i == 1:
+                    return False
+                sign = -1
+            elif sign == -1:
+                    return False
+                
+        return sign == -1
