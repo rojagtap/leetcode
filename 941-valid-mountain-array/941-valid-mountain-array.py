de6afa12559f3c031f79a2d2633ddc15680e00b7
Rohan Jagtap <rohanj30@icloud.com>
2022-08-21 01:28:57 +0530

    Time: 387 ms (23.52%), Space: 15.3 MB (97.00%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        length = len(arr)
+        
+        if length < 3:
+            return False
+        
+        sign = 1
+        for i in range(1, length):
+            if arr[i] - arr[i - 1] == 0:
+                return False
+            elif arr[i] - arr[i - 1] < 0:
+                if i == 1:
+                    return False
+                sign = -1
+            else:
+                if sign == -1:
+                    return False
+                
+        return sign == -1
