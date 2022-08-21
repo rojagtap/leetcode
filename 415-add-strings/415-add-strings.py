489cc1a40f6282828ca85464b112aebf1cdf3b57
Rohan Jagtap <rohanj30@icloud.com>
2022-08-21 19:30:46 +0530

    Time: 73 ms (37.71%), Space: 14.3 MB (18.40%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -2,16 +2,64 @@ class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         zero = ord("0")
         
-        int1 = 0
-        factor = 10
-        for num in num1:
-            digit = ord(num) - zero
-            int1 = int1 * factor + digit
+        i = len(num1) - 1
+        j = len(num2) - 1
+        
+        add = []
+        carry = 0
+        while i >= 0 and j >= 0:
+            currsum = ord(num1[i]) + ord(num2[j]) - (2 * zero) + carry
+            digit = currsum % 10
             
-        int2 = 0
-        factor = 10
-        for num in num2:
-            digit = ord(num) - zero
-            int2 = int2 * factor + digit
+            add.append(str(digit))
+            
+            carry = currsum // 10
+            
+            i -= 1
+            j -= 1
+            
+        
+        while i >= 0:
+            currsum = ord(num1[i]) - zero + carry
+            digit = currsum % 10
+            
+            add.append(str(digit))
+            
+            carry = currsum // 10
+            
+            i -= 1
+            
+        
+        while j >= 0:
+            currsum = ord(num2[j]) - zero + carry
+            digit = currsum % 10
+            
+            add.append(str(digit))
+            
+            carry = currsum // 10
+            
+            j -= 1
+            
+        if carry != 0:
+            add.append(str(carry))
+        
+        return ''.join(add)[::-1]
+
+        
+#     Solution without big ints consideration
+#     def addStrings(self, num1: str, num2: str) -> str:
+#         zero = ord("0")
+        
+#         int1 = 0
+#         factor = 10
+#         for num in num1:
+#             digit = ord(num) - zero
+#             int1 = int1 * factor + digit
+            
+#         int2 = 0
+#         factor = 10
+#         for num in num2:
+#             digit = ord(num) - zero
+#             int2 = int2 * factor + digit
                     
-        return str(int1 + int2)
+#         return str(int1 + int2)
