537ba5c8d0312f1812cba70d84d0018aa2374208
Rohan Jagtap <rohanj30@icloud.com>
2022-08-25 13:16:35 +0530

    Time: 93 ms (79.10%), Space: 13.9 MB (69.26%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,5 +1,7 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        matches = 0
+        
         freq = dict()
         for p in s1:
             freq[p] = freq.get(p, 0) + 1
@@ -8,22 +10,17 @@ class Solution:
         for r in range(len(s2)):
             if s2[r] in freq:
                 freq[s2[r]] -= 1
+                matches += (freq[s2[r]] == 0)
                 
             if r >= len(s1) - 1:
-                if self.allzero(freq):
+                if matches == len(freq):
                     return True
                 
                 if s2[l] in freq:
+                    if freq[s2[l]] == 0:
+                        matches -= 1
                     freq[s2[l]] +=  1
                     
                 l += 1
-                
-        return self.allzero(freq)
-    
-    
-    def allzero(self, freq):
-        for k in freq:
-            if freq[k] != 0:
-                return False
-            
-        return True
+
+        return matches == len(freq)
