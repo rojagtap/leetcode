e994d30cc5d74c02ac9029005afe92dadcb6a6cc
Rohan Jagtap <rohanj30@icloud.com>
2022-08-26 01:57:56 +0530

    Time: 423 ms (27.44%), Space: 16.3 MB (49.06%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -2,12 +2,14 @@ class Solution:
     def sortedSquares(self, arr: List[int]) -> List[int]:
         squares = []
 
+        # find the first non-zero number
         r = 0
         while r < len(arr) and arr[r] < 0:
             r += 1
 
         l = r - 1
 
+        # negative in reverse and positive to the right
         while l >= 0 and r < len(arr):
             while l >= 0 and arr[l] * arr[l] <= arr[r] * arr[r]:
                 squares.append(arr[l] * arr[l])
@@ -17,10 +19,12 @@ class Solution:
                 squares.append(arr[r] * arr[r])
                 r += 1
 
+        # residual l
         while l >= 0:
             squares.append(arr[l] * arr[l])
             l -= 1
-
+            
+        # residual r
         while r < len(arr):
             squares.append(arr[r] * arr[r])
             r += 1
