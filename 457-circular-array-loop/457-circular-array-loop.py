ef0f39195677e7dc50fb2e7c73061ccd6d6dab28
Rohan Jagtap <rohanj30@icloud.com>
2022-08-31 03:24:47 +0530

    Time: 753 ms (46.33%), Space: 13.9 MB (85.93%) - LeetHub

---
+++ 457-circular-array-loop/457-circular-array-loop.py
@@ -0,0 +1,35 @@
+import math
+
+class Solution:
+    def circularArrayLoop(self, arr: List[int]) -> bool:        
+        for start in range(len(arr)):
+            slow, fast = start, start
+            sign = math.copysign(1, arr[start])
+            flag = True
+            
+            while True:
+                fast = (fast + arr[fast]) % len(arr)
+                slow = (slow + arr[slow]) % len(arr)
+                
+                if sign != math.copysign(1, arr[fast]):
+                    flag = False
+                    break
+
+                if fast == (fast + arr[fast]) % len(arr):
+                    flag = False
+                    break
+                
+                fast = (fast + arr[fast]) % len(arr)
+                
+                if sign != math.copysign(1, arr[fast]):
+                    flag = False
+                    break
+                
+                if slow == fast:
+                    flag = True
+                    break
+                
+            if flag:
+                return True
+                
+        return False
