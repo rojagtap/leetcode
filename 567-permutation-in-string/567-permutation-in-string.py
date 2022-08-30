928598f03a124869569f3025a63775e49ddd56ab
Rohan Jagtap <rohanj30@icloud.com>
2022-08-30 18:05:57 +0530

    Time: 92 ms (80.11%), Space: 13.9 MB (68.73%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -11,8 +11,15 @@ class Solution:
             if s2[r] in freq:
                 freq[s2[r]] -= 1
                 matches += (freq[s2[r]] == 0)
+            else:
+                while l < r + 1:
+                    if s2[l] in freq:
+                        if freq[s2[l]] == 0:
+                            matches -= 1
+                        freq[s2[l]] +=  1
+                    l += 1
                 
-            if r >= len(s1) - 1:
+            if r - l + 1 >= len(s1):
                 if matches == len(freq):
                     return True
                 
