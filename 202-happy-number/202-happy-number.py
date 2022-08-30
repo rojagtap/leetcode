799980f64807413f9a6266b17f0411d44edef86e
Rohan Jagtap <rohanj30@icloud.com>
2022-08-30 21:21:45 +0530

    Time: 60 ms (36.38%), Space: 13.8 MB (97.11%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,30 +1,30 @@
 class Solution:
 #     O(klogn), O(1)
-#     def isHappy(self, n: int) -> bool:
-#         slow, fast = n, n
+    def isHappy(self, n: int) -> bool:
+        slow, fast = n, n
         
-#         while fast != 1:
-#             slow = self.square_sum(slow)
-#             fast = self.square_sum(self.square_sum(fast))
+        while fast != 1:
+            slow = self.square_sum(slow)
+            fast = self.square_sum(self.square_sum(fast))
             
-#             if slow == fast:
-#                 break
+            if slow == fast:
+                break
             
-#         return fast == 1
+        return fast == 1
     
 #    O(klogn), O(n)
-    def isHappy(self, n: int) -> bool:
-        hash = set()
+#     def isHappy(self, n: int) -> bool:
+#         hash = set()
         
-        while n != 1:
-            n = self.square_sum(n)
+#         while n != 1:
+#             n = self.square_sum(n)
             
-            if n in hash:
-                return False
+#             if n in hash:
+#                 return False
             
-            hash.add(n)
+#             hash.add(n)
             
-        return True
+#         return True
         
         
     def square_sum(self, n):
