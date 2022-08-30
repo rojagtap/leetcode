adcc1326af5d291eb9ebdf8d49aa16ec67adbdb7
Rohan Jagtap <rohanj30@icloud.com>
2022-08-30 22:40:10 +0530

    Time: 1577 ms (12.99%), Space: 45.2 MB (65.29%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -4,17 +4,35 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+#     O(n), O(1), recursion
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         self.slow = head
+#         return self.recurse(head)
+        
+#     def recurse(self, fast):
+#         if not fast.next:
+#             val = self.slow.val
+#             self.slow = self.slow.next
+#             return val == fast.val
+        
+#         val = self.recurse(fast.next) and self.slow.val == fast.val
+#         self.slow = self.slow.next
+        
+#         return val
+
+
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        self.slow = head
-        return self.recurse(head)
-        
-    def recurse(self, fast):
-        if not fast.next:
-            val = self.slow.val
-            self.slow = self.slow.next
-            return val == fast.val
-        
-        val = self.recurse(fast.next) and self.slow.val == fast.val
-        self.slow = self.slow.next
-        
-        return val
+        stack = []
+        while head:
+            stack.append(head.val)
+            head = head.next
+            
+        l, r = 0, len(stack) - 1
+        while l < r:
+            if stack[l] != stack[r]:
+                return False
+            
+            l += 1
+            r -= 1
+            
+        return True
