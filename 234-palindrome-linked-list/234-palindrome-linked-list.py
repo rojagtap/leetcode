09466747b110b2bbe8117fbf57ed2e7932a1d502
Rohan Jagtap <rohanj30@icloud.com>
2022-08-30 22:40:58 +0530

    Time: 2294 ms (5.00%), Space: 131 MB (5.00%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -5,34 +5,35 @@
 #         self.next = next
 class Solution:
 #     O(n), O(1), recursion
-#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-#         self.slow = head
-#         return self.recurse(head)
-        
-#     def recurse(self, fast):
-#         if not fast.next:
-#             val = self.slow.val
-#             self.slow = self.slow.next
-#             return val == fast.val
-        
-#         val = self.recurse(fast.next) and self.slow.val == fast.val
-#         self.slow = self.slow.next
-        
-#         return val
-
-
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack = []
-        while head:
-            stack.append(head.val)
-            head = head.next
+        self.slow = head
+        return self.recurse(head)
+        
+    def recurse(self, fast):
+        if not fast.next:
+            val = self.slow.val
+            self.slow = self.slow.next
+            return val == fast.val
+        
+        val = self.recurse(fast.next) and self.slow.val == fast.val
+        self.slow = self.slow.next
+        
+        return val
+
+
+#     O(n), O(n), stack
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         stack = []
+#         while head:
+#             stack.append(head.val)
+#             head = head.next
             
-        l, r = 0, len(stack) - 1
-        while l < r:
-            if stack[l] != stack[r]:
-                return False
+#         l, r = 0, len(stack) - 1
+#         while l < r:
+#             if stack[l] != stack[r]:
+#                 return False
             
-            l += 1
-            r -= 1
+#             l += 1
+#             r -= 1
             
-        return True
+#         return True
