03217b1e1062c53d71f67c3231c79aeb97b66a43
Rohan Jagtap <rohanj30@icloud.com>
2022-08-31 03:37:56 +0530

    Time: 1138 ms (24.62%), Space: 13.8 MB (85.93%) - LeetHub

--- 457-circular-array-loop/457-circular-array-loop.py
+++ 457-circular-array-loop/457-circular-array-loop.py
@@ -5,30 +5,23 @@ class Solution:
         for start in range(len(arr)):
             slow, fast = start, start
             sign = math.copysign(1, arr[start])
-            flag = True
             
             while True:
                 fast = (fast + arr[fast]) % len(arr)
                 slow = (slow + arr[slow]) % len(arr)
                 
                 if sign != math.copysign(1, arr[fast]):
-                    flag = False
                     break
 
                 if fast == (fast + arr[fast]) % len(arr):
-                    flag = False
                     break
                 
                 fast = (fast + arr[fast]) % len(arr)
                 
                 if sign != math.copysign(1, arr[fast]):
-                    flag = False
                     break
                 
                 if slow == fast:
-                    break
-                
-            if flag:
-                return True
-                
+                    return True
+                                
         return False
