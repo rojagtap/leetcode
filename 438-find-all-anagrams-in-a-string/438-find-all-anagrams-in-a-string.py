ce3533428519242e4f1c04f73f60d2cd1b6084bf
Rohan Jagtap <rohanj30@icloud.com>
2022-08-30 18:22:19 +0530

    Time: 99 ms (98.90%), Space: 15.7 MB (5.31%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -12,8 +12,16 @@ class Solution:
             if s[r] in freq:
                 freq[s[r]] -= 1
                 matches += (freq[s[r]] == 0)
+            else:
+                while l < r + 1:
+                    if s[l] in freq:
+                        if freq[s[l]] == 0:
+                            matches -= 1
+                        freq[s[l]] += 1
 
-            if r >= len(p) - 1:
+                    l += 1
+
+            if r - l + 1 >= len(p):
                 if matches == len(freq):
                     res.add(l)
 
@@ -23,8 +31,5 @@ class Solution:
                     freq[s[l]] += 1
 
                 l += 1
-
-        if matches == len(freq):
-            res.add(l)
 
         return list(res)
