46f48443ada665319d2e901ba1e1399156437c16
Rohan Jagtap <rohanj30@icloud.com>
2022-08-31 03:54:09 +0530

    Time: 2048 ms (5.05%), Space: 13.9 MB (55.35%) - LeetHub

--- 457-circular-array-loop/457-circular-array-loop.py
+++ 457-circular-array-loop/457-circular-array-loop.py
@@ -1,23 +1,50 @@
 import math
 
 class Solution:
-    def circularArrayLoop(self, arr: List[int]) -> bool:
+    # O(n2), O(1)
+    def circularArrayLoop(self, arr: List[int]) -> bool:        
         for start in range(len(arr)):
-            ptr = start
-            visited = set()
+            slow, fast = start, start
             sign = math.copysign(1, arr[start])
+            
             while True:
-                visited.add(ptr)
+                fast = (fast + arr[fast]) % len(arr)
                 
-                if ptr == (ptr + arr[ptr]) % len(arr):
+                if sign != math.copysign(1, arr[fast]):
                     break
-                    
-                ptr = (ptr + arr[ptr]) % len(arr)
+
+                if fast == (fast + arr[fast]) % len(arr):
+                    break
                 
-                if sign != math.copysign(1, arr[ptr]):
+                slow = (slow + arr[slow]) % len(arr)
+                fast = (fast + arr[fast]) % len(arr)
+                
+                if sign != math.copysign(1, arr[fast]):
                     break
-                    
-                if ptr in visited:
+                
+                if slow == fast:
                     return True
-                
+                                
         return False
+
+    # O(n2), O(n)
+#     def circularArrayLoop(self, arr: List[int]) -> bool:
+#         for start in range(len(arr)):
+#             ptr = start
+#             visited = set()
+#             sign = math.copysign(1, arr[start])
+#             while True:
+#                 visited.add(ptr)
+                
+#                 if ptr == (ptr + arr[ptr]) % len(arr):
+#                     break
+                    
+#                 ptr = (ptr + arr[ptr]) % len(arr)
+                
+#                 if sign != math.copysign(1, arr[ptr]):
+#                     break
+                    
+#                 if ptr in visited:
+#                     return True
+                
+#         return False
