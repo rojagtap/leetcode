84d0a97e748d39fc5b78d3ed0a5493b06a896741
Rohan Jagtap <rohanj30@icloud.com>
2022-09-06 12:41:21 +0530

    Time: 563 ms (45.50%), Space: 21.8 MB (88.28%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -1,14 +1,35 @@
 class Solution:
+#    O(n), O(1)
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        count = 0
-        missing = []
-        hash = set(nums)
-        nmissing = len(nums) - len(hash)
+        i = 0
+        while i < len(nums):
+            j = nums[i] - 1
+            if nums[i] != i + 1:
+                nums[i], nums[j] = nums[j], nums[i]
+                if nums[i] == nums[j]:
+                    i += 1
+            else:
+                i += 1
         
-        for num in range(1, len(nums) + 1):
-            if num not in hash:
-                count += 1
-                missing.append(num)
-                if count == nmissing:
-                    return missing
+        missing = []
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                missing.append(i + 1)
+                
+        return missing
+        
+        
+#     O(n), O(n)
+#     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+#         count = 0
+#         missing = []
+#         hash = set(nums)
+#         nmissing = len(nums) - len(hash)
+        
+#         for num in range(1, len(nums) + 1):
+#             if num not in hash:
+#                 count += 1
+#                 missing.append(num)
+#                 if count == nmissing:
+#                     return missing
                 
