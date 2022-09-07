e626193a412df93a3677221a85f09d4459bf4ed8
Rohan Jagtap <rohanj30@icloud.com>
2022-09-08 03:32:24 +0530

    Time: 52 ms (94.66%), Space: 14 MB (86.10%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,6 +1,9 @@
 class Solution:
     '''
-    
+    Since the numbers are positive integers sorted in a strictly increasing order, we can conclude that i + 1 is the ideal position for any number arr[i]
+    So if a number is not at its ideal place, it means that it is misplaced by arr[i] - (i + 1), i.e., arr[i] - (i + 1) numbers are missing so far
+    We can use binary search to look for the HIGHEST NUMBER WHICH HAS LESS MISSING NUMBERS THAN k. For example if arr is [2,3,4,7,11] and k is 2, then there is 1 missing number until 4 and 3 missing numbers at 7 so we take 4.
+    Once we have that, we can simply add the difference between k and the missing elements so far (i.e., the remaining ones) to 4 and return
     '''
 #     O(logn), O(1) binary search
     def findKthPositive(self, arr: List[int], k: int) -> int:
