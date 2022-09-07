8089b621057b35df61def269cac961090186ac8b
Rohan Jagtap <rohanj30@icloud.com>
2022-09-08 01:45:05 +0530

    Time: 517 ms (79.19%), Space: 27.3 MB (82.64%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,4 +1,6 @@
 class Solution:
+    # O(n), O(1)
+    # cyclic sort
     def firstMissingPositive(self, nums: List[int]) -> int:
         for i in range(len(nums)):
             while 1 <= nums[i] <= len(nums) and nums[i] != i + 1:
