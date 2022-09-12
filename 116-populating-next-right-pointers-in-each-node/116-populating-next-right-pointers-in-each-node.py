15742e1f32315e5895b304fa5e085f0622055d2c
Rohan Jagtap <rohanj30@icloud.com>
2022-09-12 22:23:50 +0530

    Time: 116 ms (32.57%), Space: 15.9 MB (22.35%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return root
+        
+        queue = deque([root])
+        while queue:
+            prev = None
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if prev:
+                    prev.next = node
+                    
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                    
+                prev = node
+        
+        return root
