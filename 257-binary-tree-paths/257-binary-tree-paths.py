1d68444904e37c2ce707d86b186b10eafe0d87d9
Rohan Jagtap <rohanj30@icloud.com>
2022-09-14 11:09:59 +0530

    Time: 61 ms (27.91%), Space: 13.9 MB (29.52%) - LeetHub

---
+++ 257-binary-tree-paths/257-binary-tree-paths.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        all_paths = []
+        if not root:
+            return all_paths
+        
+        self.recurse(root, [], all_paths)
+        return all_paths
+    
+    def recurse(self, node, path, all_paths):
+        path.append(str(node.val))
+        if not node.left and not node.right:
+            all_paths.append("->".join(path))
+            
+        if node.left:
+            self.recurse(node.left, path, all_paths)
+        if node.right:
+            self.recurse(node.right, path, all_paths)
+            
+        path.pop()
