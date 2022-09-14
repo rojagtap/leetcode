950349cf9fe08563d128b173ff43acca0bb8b974
Rohan Jagtap <rohanj30@icloud.com>
2022-09-14 17:57:50 +0530

    Time: 59 ms (26.24%), Space: 13.8 MB (66.16%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -5,6 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+#     # O(n), O(h), dfs stack
+#     def sumNumbers(self, root: Optional[TreeNode]) -> int:
+#         total = 0
+#         stack = [(root, 0)]
+#         while stack:
+#             node, currsum = stack.pop()
+#             currsum += node.val
+            
+#             if not node.left and not node.right:
+#                 total += currsum
+#             else:
+#                 currsum *= 10
+#                 if node.left:
+#                     stack.append((node.left, currsum))
+#                 if node.right:
+#                     stack.append((node.right, currsum))
+                    
+#         return total
+            
+    
+    # O(n), O(h), dfs recursive
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         
         def addition(node, currsum):
@@ -12,13 +33,13 @@ class Solution:
             currsum += node.val
             if not node.left and not node.right:
                 total += currsum
-                
-            currsum *= 10
-            if node.left:
-                addition(node.left, currsum)
-                
-            if node.right:
-                addition(node.right, currsum)
+            else:
+                currsum *= 10
+                if node.left:
+                    addition(node.left, currsum)
+
+                if node.right:
+                    addition(node.right, currsum)
                 
         total = 0
         addition(root, 0)
