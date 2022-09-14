a6f37ca145b5509bb71e4dc1694df152caf1d98b
Rohan Jagtap <rohanj30@icloud.com>
2022-09-14 17:58:36 +0530

    Time: 72 ms (5.64%), Space: 14 MB (19.45%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -5,43 +5,43 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-#     # O(n), O(h), dfs stack
-#     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-#         total = 0
-#         stack = [(root, 0)]
-#         while stack:
-#             node, currsum = stack.pop()
-#             currsum += node.val
-            
-#             if not node.left and not node.right:
-#                 total += currsum
-#             else:
-#                 currsum *= 10
-#                 if node.left:
-#                     stack.append((node.left, currsum))
-#                 if node.right:
-#                     stack.append((node.right, currsum))
-                    
-#         return total
-            
-    
-    # O(n), O(h), dfs recursive
+    # O(n), O(h), dfs stack
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        
-        def addition(node, currsum):
-            nonlocal total
+        total = 0
+        stack = [(root, 0)]
+        while stack:
+            node, currsum = stack.pop()
             currsum += node.val
+            
             if not node.left and not node.right:
                 total += currsum
             else:
                 currsum *= 10
                 if node.left:
-                    addition(node.left, currsum)
-
+                    stack.append((node.left, currsum))
                 if node.right:
-                    addition(node.right, currsum)
-                
-        total = 0
-        addition(root, 0)
-        
+                    stack.append((node.right, currsum))
+                    
         return total
+            
+    
+#     O(n), O(h), dfs recursive
+#     def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        
+#         def addition(node, currsum):
+#             nonlocal total
+#             currsum += node.val
+#             if not node.left and not node.right:
+#                 total += currsum
+#             else:
+#                 currsum *= 10
+#                 if node.left:
+#                     addition(node.left, currsum)
+
+#                 if node.right:
+#                     addition(node.right, currsum)
+                
+#         total = 0
+#         addition(root, 0)
+        
+#         return total
