fa6117c2905efe2f354b829f2b786d4a62d90320
Rohan Jagtap <rohanj30@icloud.com>
2022-09-15 00:14:23 +0530

    Time: 61 ms (20.88%), Space: 13.6 MB (99.98%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -5,24 +5,64 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-#    O(n), O(h), dfs stack
+#    O(n), O(1), morris traversal
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        total = 0
-        stack = [(root, 0)]
-        while stack:
-            node, currsum = stack.pop()
-            currsum += node.val
-            
-            if not node.left and not node.right:
-                total += currsum
+        total = currsum = 0
+        
+        while root:
+            if not root.left:
+                currsum = currsum * 10 + root.val
+                
+                if not root.right:
+                    total += currsum
+                
+                root = root.right
             else:
-                currsum *= 10
-                if node.left:
-                    stack.append((node.left, currsum))
-                if node.right:
-                    stack.append((node.right, currsum))
+                steps = 1
+                node = root.left
+                while node.right and node.right != root:
+                    steps += 1
+                    node = node.right
+                
+                if not node.right:
+                    node.right = root
+                    currsum = currsum * 10 + root.val
+                    root = root.left
+                else:
+                    node.right = None
+                    root = root.right
                     
+                    if not node.left:
+                        total += currsum
+                        
+                    while steps:
+                        currsum //= 10
+                        steps -= 1
+                        
         return total
+        
+        
+                    
+    
+    
+#    O(n), O(h), dfs stack
+#     def sumNumbers(self, root: Optional[TreeNode]) -> int:
+#         total = 0
+#         stack = [(root, 0)]
+#         while stack:
+#             node, currsum = stack.pop()
+#             currsum += node.val
+            
+#             if not node.left and not node.right:
+#                 total += currsum
+#             else:
+#                 currsum *= 10
+#                 if node.left:
+#                     stack.append((node.left, currsum))
+#                 if node.right:
+#                     stack.append((node.right, currsum))
+                    
+#         return total
             
     
 #     O(n), O(h), dfs recursive
