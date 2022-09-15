ffdff72b888052c7057727148e6b8c4f344d8086
Rohan Jagtap <rohanj30@icloud.com>
2022-09-15 21:01:47 +0530

    Time: 3868 ms (23.86%), Space: 15.6 MB (5.50%) - LeetHub

--- 644-maximum-average-subarray-ii/644-maximum-average-subarray-ii.py
+++ 644-maximum-average-subarray-ii/644-maximum-average-subarray-ii.py
@@ -11,6 +11,16 @@ class Solution:
     (nums[i], nums[i + 1], nums[i + 2] + ... + nums[i + m]) >= curr * m
     
     (nums[i], nums[i + 1], nums[i + 2] + ... + nums[i + m]) - curr * m >= 0
+    
+    So after picking curr, we want to check if there exists a subarray (length m >= k) such that sum(subarray) - curr * m >= 0
+    
+    For example, for an array we take a prefix sum until length k
+    
+    if the max subarray sum in the array > curr * m then we can conclude that there exists a subarray such that sum(subarray) - curr * m >= 0
+    (Note that since the length m is not known we can subtract curr from each element of nums for prefixsum so that the aggregate is sum(subarray) - curr * m. This way we dont have to do a - curr * m on every prefixsum)
+    
+    Now for max subarray sum, we maintain the min of prefixsum from the left and check if the prefixsum - minsum >= 0 
+    (If the prefixsum array is [-3, -2, -1, 1, 0], then the max sum for length 4 will be 0 - (-3), i.e. prefixsum - min)
     '''
     
     # O(nlogn) binary search
