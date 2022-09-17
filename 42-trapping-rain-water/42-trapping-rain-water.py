1e63721c6a4764fab80747e4e3245fbcfa422a36
Rohan Jagtap <rohanj30@icloud.com>
2022-09-17 17:05:57 +0530

    Time: 126 ms (95.64%), Space: 16 MB (44.45%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -15,8 +15,25 @@ class Solution:
       
     so the amount of water that current can trap is min(lmax, rmax) - height[current]
     
-    '''
-    
+    '''        
+        
+#     O(n), O(n), arrays for lmax and rmax for at each index
+#     def trap(self, height: List[int]) -> int:
+#         lmax, rmax = [height[0]], [height[len(height) - 1]] * len(height)
+        
+#         for i in range(1, len(height)):
+#             lmax.append(max(lmax[i - 1], height[i]))
+
+#         for i in range(len(height) - 2, -1, -1):
+#             rmax[i] = max(rmax[i + 1], height[i])
+            
+#         water = 0
+#         for i in range(len(height)):
+#             water += min(lmax[i], rmax[i]) - height[i]
+            
+#         return water
+
+
 #     O(n), O(1), two pointers
     def trap(self, height: List[int]) -> int:
         lmax = rmax = 0
@@ -34,20 +51,3 @@ class Solution:
                 r -= 1
         
         return water
-        
-        
-#     O(n), O(n), arrays for lmax and rmax for at each index
-#     def trap(self, height: List[int]) -> int:
-#         lmax, rmax = [height[0]], [height[len(height) - 1]] * len(height)
-        
-#         for i in range(1, len(height)):
-#             lmax.append(max(lmax[i - 1], height[i]))
-
-#         for i in range(len(height) - 2, -1, -1):
-#             rmax[i] = max(rmax[i + 1], height[i])
-            
-#         water = 0
-#         for i in range(len(height)):
-#             water += min(lmax[i], rmax[i]) - height[i]
-            
-#         return water
