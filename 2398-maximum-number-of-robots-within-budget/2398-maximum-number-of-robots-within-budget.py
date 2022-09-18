d31a0dd71ac4c833df03be56572c929a2e37ba13
Rohan Jagtap <rohanj30@icloud.com>
2022-09-18 16:24:29 +0530

    Time: 1551 ms (96.69%), Space: 22.6 MB (44.07%) - LeetHub

--- 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.py
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.py
@@ -15,62 +15,62 @@ class Solution:
     
 #     O(n), O(r) where r is max robots within budget
 #     O(n) because all the elements even though iterated thru a queue, are traversed only twice
-#     def maximumRobots(self, chargeTimes: List[int], runningCosts: List[int], budget: int) -> int:
-#         def update_max(i):
-#             while maxtimes and chargeTimes[maxtimes[-1]] < chargeTimes[i]:
-#                 maxtimes.pop()
-                
-#             maxtimes.append(i)
-            
-#         robots = 0
-#         maxtimes = deque()
-        
-#         left = 0
-#         costsum = 0
-#         for right in range(len(runningCosts)):
-#             costsum += runningCosts[right]
-#             update_max(right)
-            
-#             cost = chargeTimes[maxtimes[0]] + (right - left + 1) * costsum
-#             while cost > budget:
-#                 costsum -= runningCosts[left]
-#                 if maxtimes and maxtimes[0] == left:
-#                     maxtimes.popleft()
-                
-#                 left += 1
-#                 if left > right:
-#                     break
-                    
-#                 cost = chargeTimes[maxtimes[0]] + (right - left + 1) * costsum
-                
-#             robots = max(robots, right - left + 1)
-            
-#         return robots
-    
-#     O(nlog(r)), O(r) where r is max robots within budget
     def maximumRobots(self, chargeTimes: List[int], runningCosts: List[int], budget: int) -> int:
+        def update_max(i):
+            while maxtimes and chargeTimes[maxtimes[-1]] < chargeTimes[i]:
+                maxtimes.pop()
+                
+            maxtimes.append(i)
+            
         robots = 0
-        maxtimes = []
+        maxtimes = deque()
         
         left = 0
         costsum = 0
         for right in range(len(runningCosts)):
             costsum += runningCosts[right]
-            heappush(maxtimes, (-chargeTimes[right], right))
+            update_max(right)
             
-            cost = -maxtimes[0][0] + (right - left + 1) * costsum
+            cost = chargeTimes[maxtimes[0]] + (right - left + 1) * costsum
             while cost > budget:
                 costsum -= runningCosts[left]
+                if maxtimes and maxtimes[0] == left:
+                    maxtimes.popleft()
                 
                 left += 1
-                while maxtimes and maxtimes[0][1] < left:
-                    heappop(maxtimes)
-                
                 if left > right:
                     break
                     
-                cost = -maxtimes[0][0] + (right - left + 1) * costsum
+                cost = chargeTimes[maxtimes[0]] + (right - left + 1) * costsum
                 
             robots = max(robots, right - left + 1)
             
         return robots
+    
+#     O(nlog(r)), O(r) where r is max robots within budget
+#     def maximumRobots(self, chargeTimes: List[int], runningCosts: List[int], budget: int) -> int:
+#         robots = 0
+#         maxtimes = []
+        
+#         left = 0
+#         costsum = 0
+#         for right in range(len(runningCosts)):
+#             costsum += runningCosts[right]
+#             heappush(maxtimes, (-chargeTimes[right], right))
+            
+#             cost = -maxtimes[0][0] + (right - left + 1) * costsum
+#             while cost > budget:
+#                 costsum -= runningCosts[left]
+                
+#                 left += 1
+#                 while maxtimes and maxtimes[0][1] < left:
+#                     heappop(maxtimes)
+                
+#                 if left > right:
+#                     break
+                    
+#                 cost = -maxtimes[0][0] + (right - left + 1) * costsum
+                
+#             robots = max(robots, right - left + 1)
+            
+#         return robots
