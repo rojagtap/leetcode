ac7d15f46e9ed3cf41d71cf9d0811c601469b8b3
Rohan Jagtap <rohanj30@icloud.com>
2022-09-20 17:47:51 +0530

    Time: 879 ms (92.65%), Space: 75.7 MB (48.86%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.py
@@ -0,0 +1,74 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+    def __str__(self):
+        return "{} <--> {} <--> {}".format((self.prev.key, self.prev.value) if self.prev else None, (self.key, self.value), (self.next.key, self.next.value) if self.next else None)
+            
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.head = None
+        self.tail = None
+        
+        self.capacity = capacity
+        self.cache = dict()
+        
+    def _add(self, node):
+        node.next = self.head
+        node.prev = None
+        
+        if self.head:
+            self.head.prev = node
+        
+        self.head = node
+        
+        if not self.tail:
+            self.tail = node
+        
+    def _used(self, node):
+        self._pop(node)
+        self._add(node)
+        
+    def _pop(self, node=None):
+        if not node:
+            node = self.tail
+        
+        if node.prev:
+            node.prev.next = node.next
+        else:
+            self.head = node.next
+            
+        if node.next:
+            node.next.prev = node.prev
+        else:
+            self.tail = node.prev
+        
+        node.next = node.prev = None
+        return node
+            
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self._used(self.cache[key])
+            return self.cache[key].value
+        else:
+            return -1
+        
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache[key].value = value
+            self._used(self.cache[key])
+        else:
+            if len(self.cache) == self.capacity:
+                self.cache.pop(self._pop().key)
+                
+            self.cache[key] = Node(key, value)
+            self._add(self.cache[key])
+
+        
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
