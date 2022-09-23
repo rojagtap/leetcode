cf98f9e61a71a136c1a64ed468096a1f7880dd6d
Rohan Jagtap <rohanj30@icloud.com>
2022-09-23 17:30:55 +0530

    Time: 139 ms (16.36%), Space: 14.6 MB (97.24%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.py
+++ 784-letter-case-permutation/784-letter-case-permutation.py
@@ -1,15 +1,17 @@
 class Solution:
   def letterCasePermutation(self, s: str) -> List[str]:
-    permutations = [""]
-
-    for char in s:
-      for i in range(len(permutations)):
-        if char.isalpha():
-          if char.isupper():
-            permutations.append(permutations[i] + char.lower())
-          else:
-            permutations.append(permutations[i] + char.upper())
-        permutations[i] += char
+    permutations = []
+    permutations.append(s)
+    # process every character of the string one by one
+    for i in range(len(s)):
+      if s[i].isalpha():  # only process characters, skip digits
+        # we will take all existing permutations and change the letter case appropriately
+        n = len(permutations)
+        for j in range(n):
+          chs = list(permutations[j])
+          # if the current character is in upper case, change it to lower case or vice versa
+          chs[i] = chs[i].swapcase()
+          permutations.append(''.join(chs))
 
     return permutations
         
