9f62e3e0b7c83a3587c67c4039560e43ce99e951
Rohan Jagtap <rohanj30@icloud.com>
2022-09-23 16:59:42 +0530

    Time: 71 ms (42.59%), Space: 14 MB (58.63%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,17 +1,21 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-      permutations = []
-      def perms(nums, curr):
-        if len(nums) == 1:
-          permutations.append(curr + [next(iter(nums))])
-          return
+      numsLength = len(nums)
+      result = []
+      permutations = deque()
+      permutations.append([])
+      for currentNumber in nums:
+        # we will take all existing permutations and add the current number to create new permutations
+        n = len(permutations)
+        for _ in range(n):
+          oldPermutation = permutations.popleft()
+          # create a new permutation by adding the current number at every position
+          for j in range(len(oldPermutation)+1):
+            newPermutation = list(oldPermutation)
+            newPermutation.insert(j, currentNumber)
+            if len(newPermutation) == numsLength:
+              result.append(newPermutation)
+            else:
+              permutations.append(newPermutation)
 
-        for num in set(nums):
-          nums.remove(num)
-          curr.append(num)
-          perms(nums, curr)
-          curr.pop()
-          nums.add(num)
-
-      perms(set(nums), [])
-      return permutations
+      return result
