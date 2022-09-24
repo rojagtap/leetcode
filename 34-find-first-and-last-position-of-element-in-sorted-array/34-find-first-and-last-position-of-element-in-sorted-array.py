c6538aa1e7da3e98be3a9c5270b6dd9eb8945c23
Rohan Jagtap <rohanj30@icloud.com>
2022-09-24 12:35:04 +0530

    Time: 169 ms (28.67%), Space: 15.4 MB (48.41%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,19 +1,25 @@
 class Solution:
     def searchRange(self, arr: List[int], key: int) -> List[int]:
-        def search(start, end):
-          left, right = -1, -1
-          l, r = start, end
-          while l <= r:
-            mid = l + (r - l) // 2
-            if key < arr[mid]:
-              r = mid - 1
-            elif key > arr[mid]:
-              l = mid + 1
+      def binary_search(arr, key, findMaxIndex):
+        keyIndex = -1
+        start, end = 0, len(arr) - 1
+        while start <= end:
+          mid = start + (end - start) // 2
+          if key < arr[mid]:
+            end = mid - 1
+          elif key > arr[mid]:
+            start = mid + 1
+          else:  # key == arr[mid]
+            keyIndex = mid
+            if findMaxIndex:
+              start = mid + 1  # search ahead to find the last index of 'key'
             else:
-              left = search(start, mid - 1)[0] if mid - 1 >= 0 and arr[mid - 1] == key else mid
-              right = search(mid + 1, end)[1] if mid + 1 <= end and arr[mid + 1] == key else mid
-              break
+              end = mid - 1  # search behind to find the first index of 'key'
 
-          return [left, right]
-          
-        return search(0, len(arr) - 1)
+        return keyIndex
+
+      result = [- 1, -1]
+      result[0] = binary_search(arr, key, False)
+      if result[0] != -1:  # no need to search, if 'key' is not present in the input array
+        result[1] = binary_search(arr, key, True)
+      return result
