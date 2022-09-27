15a5d4996436102094426d37317f6cf4c7929b32
Rohan Jagtap <rohanj30@icloud.com>
2022-09-27 22:35:16 +0530

    Time: 160 ms (61.88%), Space: 15.9 MB (31.18%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -3,19 +3,18 @@ import math
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        prefixsum = [[0 for j in range(cols + 1)] for i in range(rows + 1)]
         
-        for i in range(1, rows + 1):
-            prefixsum[i][1] = prefixsum[i - 1][1] + grid[i - 1][0]
+        for i in range(1, rows):
+            grid[i][0] += grid[i - 1][0]
                 
-        for j in range(1, cols + 1):
-            prefixsum[1][j] = prefixsum[1][j - 1] + grid[0][j - 1]
+        for j in range(1, cols):
+            grid[0][j] += grid[0][j - 1]
             
-        for i in range(2, rows + 1):
-            for j in range(2, cols + 1):
-                prefixsum[i][j] = grid[i - 1][j - 1] + min(prefixsum[i - 1][j], prefixsum[i][j - 1])
+        for i in range(1, rows):
+            for j in range(1, cols):
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
         
-        return prefixsum[rows][cols]
+        return grid[rows - 1][cols - 1]
     
 #     # O(2n), O(n), dfs
 #     def minPathSum(self, grid: List[List[int]]) -> int:
