028305e49cdfb0c70a6a339d9eb0de2add052796
Rohan Jagtap <rohanj30@icloud.com>
2022-09-28 22:38:14 +0530

    Time: 256 ms, Memory: 14.6 MB - LeetHub

---
+++ minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,51 @@
+import math
+import collections
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        freq = collections.Counter(t)
+        minlen = math.inf
+        
+        start, end = 0, len(s)
+        matches = 0
+        left = 0
+        for right in range(len(s)):
+            if s[right] in freq:
+                freq[s[right]] -= 1
+                if freq[s[right]] == 0:
+                    matches += 1
+                    
+            if matches == len(freq):
+                while left < right:
+                    if s[left] in freq:
+                        if freq[s[left]] == 0:
+                            break
+                            
+                        freq[s[left]] += 1
+                        
+                    left += 1
+                    
+                if (right - left + 1) < minlen:
+                    start, end = left, right
+                    minlen = right - left + 1
+                    
+#             or
+#             while matches == len(freq):
+#                 if (right - left + 1) < minlen:
+#                     start, end = left, right
+#                     minlen = right - left + 1
+                    
+#                 if s[left] in freq:
+#                     if freq[s[left]] == 0:
+#                         matches -= 1
+                    
+#                     freq[s[left]] += 1
+                
+#                 left += 1
+                
+        if minlen != math.inf:
+            return s[start: end + 1]
+        else:
+            return ""
+        
