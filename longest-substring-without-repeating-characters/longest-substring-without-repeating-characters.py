c00de50c1008ae679abfc7af1fa5dc90dd08b6e3
Rohan Jagtap <rohanj30@icloud.com>
2022-09-28 19:43:47 +0530

    Time: 167 ms, Memory: 14.2 MB - LeetHub

---
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -0,0 +1,57 @@
+class Solution:
+    '''
+    O(n)
+    Start with left and right pointers starting at 0, 0
+    
+    1. Move the right pointer across the string and add each character to a hash
+    2. while the character encountered is present in hash (meaning there is a repeated element), shrink the substring from the back, until the repeated element is excluded. Also remove all the elements in between from the hash as we are starting with a fresh new substring
+    
+    Repeat steps 1 and 2 until the right pointer reaches the end of the string
+    '''
+    
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        hash = set()
+        length = len(s)
+        longest = 1
+        l = 0
+        
+        if length == 0 or length == 1:
+            return length
+                    
+        for r in range(length):
+            while s[r] in hash:
+                hash.remove(s[l])
+                l += 1
+                
+            hash.add(s[r])
+            longest = max(longest, (r + 1) - l)
+
+        
+        return longest
+        
+#     O(n2)
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+
+#         length = len(s)
+        
+#         if length == 0 or length == 1:
+#             return length
+        
+#         substring = 1
+#         const = "placeholder"
+        
+#         for i in range(length - 1):
+#             h = {s[i]: const}
+#             temp = 1
+#             for j in range(i + 1, length):
+#                 if not h.get(s[j], None):
+#                     h[s[j]] = const
+#                     temp += 1
+#                 else: 
+#                     break
+            
+#             if temp > substring:
+#                 substring = temp
+                
+#         return substring
+        
