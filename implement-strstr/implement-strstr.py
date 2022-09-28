46b028ad721502d6b915d28b013b0d4b4ba18bdd
Rohan Jagtap <rohanj30@icloud.com>
2022-09-28 21:26:15 +0530

    Time: 43 ms, Memory: 13.9 MB - LeetHub

---
+++ implement-strstr/implement-strstr.py
@@ -0,0 +1,41 @@
+class Solution:
+#     KMP
+    def get_failure_function(self, needle):
+        i, j, n = 1, 0, len(needle)
+        failureFunction = [0] * len(needle)
+        while i < n:
+            if needle[j] == needle[i]:
+                failureFunction[i] = j + 1
+                i += 1
+                j += 1
+            elif needle[j] != needle[i] and j > 0:
+                j = failureFunction[j - 1]
+            else:
+                failureFunction[i] = 0
+                i += 1
+
+        return failureFunction
+
+    def KMP(self, failureFunction, haystack, needle):
+        i, j, = 0, 0
+        n, m = len(haystack), len(needle)
+
+        while i < n and j < m:
+            if haystack[i] == needle[j]:
+                i += 1
+                j += 1
+            
+            if j == m:
+                return i - j
+            elif i < n and j < m and haystack[i] != needle[j]:
+                if j != 0:
+                    j = failureFunction[j - 1]
+                else:
+                    i += 1
+
+        return -1
+    
+    
+    def strStr(self, haystack: str, needle: str) -> int:
+        failureFunction = self.get_failure_function(needle)
+        return self.KMP(failureFunction, haystack, needle)        
