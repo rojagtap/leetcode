add30851842e87111d0779c55b40ba255dcfe8f1
Rohan Jagtap <rohanj30@icloud.com>
2022-09-28 22:27:51 +0530

    Time: 219 ms (30.14%), Space: 18.5 MB (42.99%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,41 @@
+class Solution:
+#     O(gk), g is number of groups, k is str len
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         def is_anagram(a, b):
+#             if not len(a) == len(b):
+#                 return False
+            
+#             matches = 0
+#             counts = Counter(b)
+#             for c in a:
+#                 if c not in counts:
+#                     return False
+#                 else:
+#                     counts[c] -= 1
+#                     if counts[c] == 0:
+#                         matches += 1
+            
+#             return matches == len(counts)
+                
+#         groups = []
+#         for i in range(len(strs)):
+#             if strs[i] == 0:
+#                 continue
+            
+#             group = [strs[i]]
+#             for j in range(i + 1, len(strs)):
+#                 if strs[j] != 0 and is_anagram(strs[i], strs[j]):
+#                     group.append(strs[j])
+#                     strs[j] = 0
+            
+#             groups.append(group)
+        
+#         return groups
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for s in strs:
+            groups[tuple(sorted(s))].append(s)
+        
+        return groups.values()
+            
