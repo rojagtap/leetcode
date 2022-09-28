1638bf8b1821debf8e04b781eed48db538ca38af
Rohan Jagtap <rohanj30@icloud.com>
2022-09-29 00:48:29 +0530

    Time: 119 ms (40.86%), Space: 18.1 MB (60.04%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -1,37 +1,60 @@
-import heapq
+import heapq, math
 
 class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.heap = []
-        self.size = 0
-        
+        self.min = [inf]        
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        heapq.heappush(self.heap, val)
-        self.size += 1
+        self.min.append(min(self.min[-1], val))
 
     def pop(self) -> None:
-        val = self.stack.pop()
-        self.size -= 1
-        
-        idx = self.heap.index(val)
-        self.heap[idx] = self.heap[-1]
-        self.heap.pop()
-        
-        if idx < self.size:
-            heapq._siftup(self.heap, idx)
-            heapq._siftdown(self.heap, 0, idx)
-            
+        self.min.pop()
+        self.stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
         
 
     def getMin(self) -> int:
-        return self.heap[0]
+        return self.min[-1]
+
+
+# O(logn), O(n), O(1), O(1), using heap
+# class MinStack:
+
+#     def __init__(self):
+#         self.stack = []
+#         self.heap = []
+#         self.size = 0
+        
+
+#     def push(self, val: int) -> None:
+#         self.stack.append(val)
+#         heapq.heappush(self.heap, val)
+#         self.size += 1
+
+#     def pop(self) -> None:
+#         val = self.stack.pop()
+#         self.size -= 1
+        
+#         idx = self.heap.index(val)
+#         self.heap[idx] = self.heap[-1]
+#         self.heap.pop()
+        
+#         if idx < self.size:
+#             heapq._siftup(self.heap, idx)
+#             heapq._siftdown(self.heap, 0, idx)
+            
+
+#     def top(self) -> int:
+#         return self.stack[-1]
+        
+
+#     def getMin(self) -> int:
+#         return self.heap[0]
         
 
 
