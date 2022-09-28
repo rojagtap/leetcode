7511940de9044b24e5e363e0dc2d6447cd39ff41
Rohan Jagtap <rohanj30@icloud.com>
2022-09-28 20:16:25 +0530

    Time: 788 ms, Memory: 14.3 MB - LeetHub

---
+++ 3sum-closest/3sum-closest.py
@@ -0,0 +1,64 @@
+import math
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        nums.sort() 
+        best = inf
+        
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            j = i + 1
+            k = n - 1
+            
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+
+                if abs(s - target) < abs(best - target):
+                    best = s
+                
+                m = (j + k) // 2
+
+                if s > target:
+                    if m != k and nums[m] > (target - nums[i] - nums[j]):
+                        k = m
+                    else:
+                        k -= 1
+                elif s < target:
+                    if m != j and nums[m] < (target - nums[i] - nums[k]):
+                        j = m
+                    else:
+                        j += 1
+                else:
+                    return target
+                    
+        return best
+
+#     def threeSumClosest(self, nums: List[int], target: int) -> int:
+#         length = len(nums)
+#         diff = math.inf
+#         nums.sort()
+        
+#         for i in range(length - 2):
+#             if i > 0 and nums[i] == nums[i - 1]:
+#                 continue
+            
+#             l = i + 1
+#             r = length - 1
+#             while l < r:
+#                 currsum = nums[i] + nums[l] + nums[r]
+                
+#                 if abs(target - currsum) < abs(diff):
+#                     diff = target - currsum
+#                 elif currsum == target:
+#                     return target
+                    
+#                 if currsum < target:
+#                     l += 1
+#                 else:
+#                     r -= 1
+                    
+#         return target - diff
+                
