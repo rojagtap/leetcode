6a550b8acccbd20b9703ccb4b87800c7bcaee8d5
Rohan Jagtap <rohanj30@icloud.com>
2022-09-29 12:05:41 +0530

    Time: 622 ms (28.20%), Space: 25.2 MB (96.23%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,11 +1,26 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        def reverse(i, j):
+            nonlocal nums
+            
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
         
-        queue = deque(nums)
-        queue.rotate(k % len(nums))
+        n = len(nums)
+        k = k % n
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
         
-        for i in range(len(nums)):
-            nums[i] = queue[i]
+#     def rotate(self, nums: List[int], k: int) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+        
+#         queue = deque(nums)
+#         queue.rotate(k % len(nums))
+        
+#         for i in range(len(nums)):
+#             nums[i] = queue[i]
