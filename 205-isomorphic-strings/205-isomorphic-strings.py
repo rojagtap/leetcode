50e490ba782552aec8bf5fa6320abe86496cfa02
Rohan Jagtap <rohanj30@icloud.com>
2022-09-29 12:38:14 +0530

    Time: 101 ms (16.34%), Space: 14.3 MB (45.57%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        mapping = dict()
+        tset = set()
+        for i in range(len(s)):
+            if (s[i] in mapping and t[i] != mapping[s[i]]) or (s[i] not in mapping and t[i] in tset):
+                return False
+                
+            mapping[s[i]] = t[i]
+            tset.add(t[i])
+            
+        return True
+        
+        
