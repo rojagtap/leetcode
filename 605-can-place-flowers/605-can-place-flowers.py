f8bc930f14cc563bfffe22f5f8f80c79c3b949a8
Rohan Jagtap <rohanj30@icloud.com>
2022-09-29 13:13:01 +0530

    Time: 485 ms (5.04%), Space: 14.5 MB (32.20%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        i = 0
+        while i < len(flowerbed) and n:
+            if flowerbed[i]:
+                i += 2
+            elif (i + 1) < len(flowerbed) and flowerbed[i + 1]:
+                i += 1
+            else:
+                n -= 1
+                i += 2
+                
+        return n == 0
