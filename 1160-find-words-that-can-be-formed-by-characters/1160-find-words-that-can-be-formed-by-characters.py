10461a888fbfd10b6206b7447026f7ca918db6f2
Rohan Jagtap <rohanj30@icloud.com>
2022-09-30 20:46:29 +0530

    Time: 392 ms (30.91%), Space: 14.5 MB (36.97%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -1,4 +1,6 @@
 class Solution:
+    # O(k + nm), O(nm) where n is the number of words, k is the total number of characters in words, m is the number of distinct characters in chars
+    # nm because for each word we are making a copy of the chars counter which is m time and m space
     def countCharacters(self, words: List[str], chars: str) -> int:
         def good(word, chars):
             for char in word:
