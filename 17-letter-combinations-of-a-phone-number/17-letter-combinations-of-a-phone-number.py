323cd2f05f6fee7a5027d294c91ccb1d5c209abd
Rohan Jagtap <rohanj30@icloud.com>
2022-10-01 00:45:56 +0530

    Time: 63 ms (18.90%), Space: 13.8 MB (79.24%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -10,48 +10,36 @@ class Solution:
             "9": "wxyz"
     }
     
+#     O(n), O(n) where n is the total number of combinations possible
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return ""
+                            
+        combinations = list(self.mapping[digits[0]])
         
-        chars = [self.mapping[digit] for digit in digits]
-        combinations = []
-        for product in itertools.product(*chars):
-            combinations.append("".join(product))
-            
+        for i in range(1, len(digits)):
+            diff = len(combinations)
+            combinations *= len(self.mapping[digits[i]])
+            for j in range(len(self.mapping[digits[i]])):
+                c = self.mapping[digits[i]][j]
+                start = j * diff
+                end = start + diff
+                for k in range(start, end):
+                    combinations[k] += c
+                
         return combinations
         
-        
-#     # O(n), O(n) where n is the total number of combinations possible
+#     Using library
 #     def letterCombinations(self, digits: str) -> List[str]:
 #         if not digits:
 #             return ""
         
-        # mapping = {
-        #     "2": "abc",
-        #     "3": "def",
-        #     "4": "ghi",
-        #     "5": "jkl",
-        #     "6": "mno",
-        #     "7": "pqrs",
-        #     "8": "tuv",
-        #     "9": "wxyz"
-        # }
-                    
-#         combinations = list(mapping[digits[0]])
-        
-#         for i in range(1, len(digits)):
-#             diff = len(combinations)
-#             combinations *= len(mapping[digits[i]])
-#             for j in range(len(mapping[digits[i]])):
-#                 c = mapping[digits[i]][j]
-#                 start = j * diff
-#                 end = start + diff
-#                 for k in range(start, end):
-#                     combinations[k] += c
-                
+#         chars = [self.mapping[digit] for digit in digits]
+#         combinations = []
+#         for product in itertools.product(*chars):
+#             combinations.append("".join(product))
+            
 #         return combinations
-        
-        
+
         
         
