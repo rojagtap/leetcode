894aeb6fe2880a1920288e3575764b8fa657d273
Rohan Jagtap <rohanj30@icloud.com>
2022-09-30 15:17:34 +0530

    Time: 505 ms (41.64%), Space: 45.6 MB (5.01%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.py
@@ -0,0 +1,48 @@
+DELETED = -1
+
+class MyHashSet:
+
+    def __init__(self):
+        self.size = 10 ** 6
+        self.nums = [None] * self.size
+
+    def add(self, key: int) -> None:
+        idx = i = 0
+        while True:
+            idx = (self.__hash1(key) + i * self.__hash2(key)) % self.size
+            i += 1
+            if self.nums[idx] is None or self.nums[idx] == DELETED or self.nums[idx] == key:
+                self.nums[idx] = key
+                break
+                                
+    def __find(self, key):
+        idx = i = 0
+        while True:
+            idx = (self.__hash1(key) + i * self.__hash2(key)) % self.size
+            i += 1
+            
+            if idx >= self.size or self.nums[idx] is None or self.nums[idx] == key:
+                break
+            
+        return idx if self.nums[idx] == key else None
+
+    def remove(self, key: int) -> None:
+        idx = self.__find(key)
+        if idx is not None:
+            self.nums[idx] = DELETED
+
+    def contains(self, key: int) -> bool:
+        return self.__find(key) is not None
+    
+    def __hash1(self, val):
+        return val % self.size
+    
+    def __hash2(self, val):
+        return (7+ val * 3) % self.size
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
