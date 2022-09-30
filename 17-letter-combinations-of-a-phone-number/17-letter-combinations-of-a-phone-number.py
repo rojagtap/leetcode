ce57466f27cfb3f98baf511644233cfddcd060b3
Rohan Jagtap <rohanj30@icloud.com>
2022-10-01 00:46:39 +0530

    Time: 54 ms (44.75%), Space: 13.8 MB (79.24%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,5 +1,21 @@
 class Solution:
-    mapping = {
+#     mapping = {
+#             "2": "abc",
+#             "3": "def",
+#             "4": "ghi",
+#             "5": "jkl",
+#             "6": "mno",
+#             "7": "pqrs",
+#             "8": "tuv",
+#             "9": "wxyz"
+#     }
+    
+#     O(n), O(n) where n is the total number of combinations possible
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return ""
+        
+        mapping = {
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -8,20 +24,15 @@ class Solution:
             "7": "pqrs",
             "8": "tuv",
             "9": "wxyz"
-    }
-    
-#     O(n), O(n) where n is the total number of combinations possible
-    def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return ""
-                            
-        combinations = list(self.mapping[digits[0]])
+        }
+                    
+        combinations = list(mapping[digits[0]])
         
         for i in range(1, len(digits)):
             diff = len(combinations)
-            combinations *= len(self.mapping[digits[i]])
-            for j in range(len(self.mapping[digits[i]])):
-                c = self.mapping[digits[i]][j]
+            combinations *= len(mapping[digits[i]])
+            for j in range(len(mapping[digits[i]])):
+                c = mapping[digits[i]][j]
                 start = j * diff
                 end = start + diff
                 for k in range(start, end):
