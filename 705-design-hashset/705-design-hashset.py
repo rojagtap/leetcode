899979d2098441356f83effb4b47c8faa75f9363
Rohan Jagtap <rohanj30@icloud.com>
2022-09-30 15:54:29 +0530

    Time: 210 ms (89.54%), Space: 20.6 MB (23.01%) - LeetHub

--- 705-design-hashset/705-design-hashset.py
+++ 705-design-hashset/705-design-hashset.py
@@ -7,15 +7,25 @@ class MyHashSet:
         self.size = 10 ** 5
         self.nums = [None] * self.size
 
-    # public methods
     def add(self, key: int) -> None:
         idx = i = 0
         while True:
-            idx = self.__get_index(key, i)
+            idx = (self.__hash1(key) + i * self.__hash2(key)) % self.size
             i += 1
             if self.nums[idx] is None or self.nums[idx] == DELETED or self.nums[idx] == key:
                 self.nums[idx] = key
                 break
+                                
+    def __find(self, key):
+        idx = i = 0
+        while True:
+            idx = (self.__hash1(key) + i * self.__hash2(key)) % self.size
+            i += 1
+            
+            if idx >= self.size or self.nums[idx] is None or self.nums[idx] == key:
+                break
+            
+        return idx if self.nums[idx] == key else None
 
     def remove(self, key: int) -> None:
         idx = self.__find(key)
@@ -25,27 +35,11 @@ class MyHashSet:
     def contains(self, key: int) -> bool:
         return self.__find(key) is not None
     
+    def __hash1(self, val):
+        return val % self.size
     
-    # private methods
-    def __get_index(self, key, i):
-        return (self.__hash1(key) + i * self.__hash2(key)) % self.size
-    
-    def __find(self, key):
-        idx = i = 0
-        while True:
-            idx = self.__get_index(key, i)
-            i += 1
-            
-            if idx >= self.size or self.nums[idx] is None or self.nums[idx] == key:
-                break
-            
-        return idx if self.nums[idx] == key else None
-    
-    def __hash1(self, key):
-        return key % self.size
-    
-    def __hash2(self, key):
-        return (1 + key * 3) % self.size
+    def __hash2(self, val):
+        return (1 + val * 3) % self.size
 
 
 # Your MyHashSet object will be instantiated and called as such:
