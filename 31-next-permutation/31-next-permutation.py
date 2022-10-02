e25411735fb0b2e3b48f78e3f4b96d704e45fea2
Rohan Jagtap <rohanj30@icloud.com>
2022-10-02 16:30:20 +0530

    Time: 83 ms (33.86%), Space: 14 MB (24.83%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        i = len(nums) - 1
+        while i > 0 and nums[i] <= nums[i - 1]:
+            i -= 1
+        
+        if i:
+            mindiff = i
+            for j in range(i, len(nums)):
+                if nums[j] > nums[i - 1] and nums[j] - nums[i - 1] < nums[mindiff] - nums[i - 1]:
+                    mindiff = j
+
+            nums[i - 1], nums[mindiff] = nums[mindiff], nums[i - 1]
+        
+        nums[i:] = sorted(nums[i:])
+            
