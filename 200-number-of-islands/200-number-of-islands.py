dc7cecb60a1fe5c8cf583d17b2c3734cb38d0a61
Rohan Jagtap <rohanj30@icloud.com>
2022-10-03 22:58:55 +0530

    Time: 790 ms (21.45%), Space: 24 MB (12.34%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        
+        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        
+        lands = [(i, j) for j in range(cols) for i in range(rows) if grid[i][j] == '1']
+        visited = {lands[i]: False for i in range(len(lands))}
+        
+        islands = 0
+        for land in lands:
+            if visited[land]:
+                continue
+            
+            islands += 1
+            visited[land] = True
+            queue = deque([land])
+            while queue:
+                i, j = queue.popleft()
+                
+                for x, y in directions:
+                    nextx, nexty = i + x, j + y
+                    if 0 <= nextx < rows and 0 <= nexty < cols and grid[nextx][nexty] == '1' and not visited[(nextx, nexty)]:
+                        queue.append((nextx, nexty))
+                        visited[(nextx, nexty)] = True
+        
+        return islands
+                        
+                
+        
