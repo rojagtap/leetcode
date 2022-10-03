408b882cf503ba1fb202a0b437d76a35845460b5
Rohan Jagtap <rohanj30@icloud.com>
2022-10-03 19:28:53 +0530

    Time: 79 ms (38.58%), Space: 13.8 MB (67.79%) - LeetHub

--- 179-largest-number/179-largest-number.py
+++ 179-largest-number/179-largest-number.py
@@ -2,8 +2,8 @@ class LargerNumKey(str):
     def __lt__(x, y):
         return x + y < y + x
 
-
 class Solution:
+    # O(nlogn), O(n)
     def largestNumber(self, nums: List[int]) -> str:
         largest = "".join(sorted([str(num) for num in nums], key=LargerNumKey, reverse=True))
         return largest if largest[0] != '0' else '0'
