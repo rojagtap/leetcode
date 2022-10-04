1f560f94ce629f1093c1ba52bb6f92dff8b901a6
Rohan Jagtap <rohanj30@icloud.com>
2022-10-04 23:52:38 +0530

    Time: 9324 ms (6.20%), Space: 17.6 MB (71.66%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,17 +1,38 @@
 class Solution:
-    # O(1), O(1)
+    # O(8^min(x, y)), O(1)
     def minKnightMoves(self, x: int, y: int) -> int:
         x, y = abs(x), abs(y)
-        if x < y:
-            x, y = y, x
+        directions = [[1, 2], [-1, 2], [2, 1], [2, -1], [-1, -2], [1, -2], [-2, 1], [-2, -1]]
         
-        if x == 1 and y == 0:
-            return 3        
-        elif x == 2 and y == 2:
-            return 4   
+        visited = [[0 for _ in range(600)] for _ in range(600)]
+        visited[0][0] = 1
         
-        delta = x - y
-        if y > delta:
-            return delta - 2 * math.floor((delta - y) // 3)
-        else:
-            return delta - 2 * math.floor((delta - y) // 4)
+        queue = deque([(0, 0, 0)])
+        while queue:
+            curr_x, curr_y, moves = queue.popleft()
+            if curr_x == x and curr_y == y:
+                return moves
+            
+            for _x, _y in directions:
+                next_x, next_y = (curr_x + _x, curr_y + _y)
+                if not visited[next_x][next_y]:
+                    queue.append((next_x, next_y, moves + 1))
+                    visited[next_x][next_y] = 1
+                    
+                    
+#     # O(1), O(1)
+#     def minKnightMoves(self, x: int, y: int) -> int:
+#         x, y = abs(x), abs(y)
+#         if x < y:
+#             x, y = y, x
+        
+#         if x == 1 and y == 0:
+#             return 3        
+#         elif x == 2 and y == 2:
+#             return 4   
+        
+#         delta = x - y
+#         if y > delta:
+#             return delta - 2 * math.floor((delta - y) // 3)
+#         else:
+#             return delta - 2 * math.floor((delta - y) // 4)
