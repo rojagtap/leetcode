94d0bdae699c5390832b272e002b458c15c230a1
Rohan Jagtap <rohanj30@icloud.com>
2022-10-05 21:12:04 +0530

    Time: 240 ms (24.39%), Space: 15.6 MB (61.18%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    # Topological sort
+    # O(V + E), O(V + E) where V is # of vertices (courses), E is the # of edges (prerequisites)
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        order = []
+        if numCourses <= 0:
+            return order
+        
+        indegrees = {i: 0 for i in range(numCourses)}
+        adjacency = defaultdict(list)
+        
+        for post, pre in prerequisites:
+            indegrees[post] += 1
+            adjacency[pre].append(post)
+            
+        sources = deque([course for course, pres in indegrees.items() if pres == 0])
+        while sources:
+            course = sources.popleft()
+            order.append(course)
+            
+            for next_course in adjacency[course]:
+                indegrees[next_course] -= 1
+                if indegrees[next_course] == 0:
+                    sources.append(next_course)
+                    
+        return order if len(order) == numCourses else []
