2f3053b9ec255e0436ea30f106c55968793193ee
Rohan Jagtap <rohanj30@icloud.com>
2022-10-06 01:17:36 +0530

    Time: 731 ms (94.31%), Space: 20.2 MB (6.47%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.py
+++ 630-course-schedule-iii/630-course-schedule-iii.py
@@ -1,21 +1,23 @@
 class Solution:
     # O(nlogn), O(n), two heaps
     def scheduleCourse(self, courses: List[List[int]]) -> int:
+        courses.sort(key=lambda course: course[1])
         max_duration = []
-        min_last_day = []
-        for duration, day in courses:
-            heappush(min_last_day, (day, duration))
+        # min_last_day = []
+        # for duration, day in courses:
+        #     heappush(min_last_day, (day, duration))
         
         days = 0
-        while min_last_day:
-            earliest = heappop(min_last_day)
+        for earliest in courses:
+        # while min_last_day:
+            # earliest = heappop(min_last_day)
             
-            if days + earliest[1] <= earliest[0]:
-                days += earliest[1]
-                heappush(max_duration, -earliest[1])
-            elif max_duration and -max_duration[0] > earliest[1]:
-                days += heappop(max_duration) + earliest[1]
-                heappush(max_duration, -earliest[1])
+            if days + earliest[0] <= earliest[1]:
+                days += earliest[0]
+                heappush(max_duration, -earliest[0])
+            elif max_duration and -max_duration[0] > earliest[0]:
+                days += heappop(max_duration) + earliest[0]
+                heappush(max_duration, -earliest[0])
                 
         return len(max_duration)
             
