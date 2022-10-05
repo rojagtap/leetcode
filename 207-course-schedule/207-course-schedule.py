09edc05c0c8e5d9873416c80eff11546f5d3a606
Rohan Jagtap <rohanj30@icloud.com>
2022-10-05 18:11:49 +0530

    Time: 102 ms (94.68%), Space: 15.5 MB (89.74%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,26 @@
+class Solution:
+#     O(n), O(n)
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if numCourses <= 0:
+            return False
+        
+        indegrees = {i: 0 for i in range(numCourses)}
+        adjacency = defaultdict(list)
+        
+        for post, pre in prerequisites:
+            indegrees[post] += 1
+            adjacency[pre].append(post)
+            
+        can_take = 0
+        sources = deque([course for course, pres in indegrees.items() if pres == 0])
+        while sources:
+            can_take += 1
+            course = sources.popleft()
+            
+            for next_course in adjacency[course]:
+                indegrees[next_course] -= 1
+                if indegrees[next_course] == 0:
+                    sources.append(next_course)
+                    
+        return can_take == numCourses
+            
