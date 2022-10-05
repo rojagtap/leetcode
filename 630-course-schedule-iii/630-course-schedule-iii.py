33ca7af7f3df70ba48c6bb674dd83e35d38931f5
Rohan Jagtap <rohanj30@icloud.com>
2022-10-06 01:17:59 +0530

    Time: 666 ms (100.00%), Space: 19.4 MB (93.33%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.py
+++ 630-course-schedule-iii/630-course-schedule-iii.py
@@ -1,17 +1,11 @@
 class Solution:
-    # O(nlogn), O(n), two heaps
+    # O(nlogn), O(n)
     def scheduleCourse(self, courses: List[List[int]]) -> int:
         courses.sort(key=lambda course: course[1])
         max_duration = []
-        # min_last_day = []
-        # for duration, day in courses:
-        #     heappush(min_last_day, (day, duration))
-        
+
         days = 0
-        for earliest in courses:
-        # while min_last_day:
-            # earliest = heappop(min_last_day)
-            
+        for earliest in courses:            
             if days + earliest[0] <= earliest[1]:
                 days += earliest[0]
                 heappush(max_duration, -earliest[0])
