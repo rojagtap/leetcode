a6c6d33119ff51deb9035bababeee99687c539ad
Rohan Jagtap <rohanj30@icloud.com>
2022-10-07 17:34:30 +0530

    Time: 35 ms (98.72%), Space: 14.2 MB (9.67%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -0,0 +1,20 @@
+class Solution:
+    # backtracking with memoization
+    # O(n * u), O(u) where u is the number of unique targets remaining vs the corresp count returned
+    def combinationSum4(self, candidates: List[int], target: int) -> int:
+        @lru_cache(maxsize = None)
+        def backtrack(target):
+            count = 0            
+            if target > 0:
+                i = 0
+                while i < len(candidates) and target - candidates[i] >= 0:
+                    count += backtrack(target - candidates[i])
+                    i += 1
+                    
+                return count
+            else:
+                return 1
+        
+        candidates.sort()
+        
+        return backtrack(target)
