b8284cf27d206ba8e6afc0b523cad36a5e6222bf
Rohan Jagtap <rohanj30@icloud.com>
2022-10-07 10:37:45 +0530

    Time: 127 ms (12.70%), Space: 14 MB (48.03%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -0,0 +1,36 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        
+        rotten = empty = 0
+        queue = deque()
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+                    rotten += 1
+                elif grid[i][j] == 0:
+                    empty += 1
+                
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        
+        
+        if rotten == (rows * cols) - empty:
+            return 0
+        
+        minutes = -1
+        while queue:
+            for _ in range(len(queue)):
+                i, j = queue.popleft()
+                
+                for r, c in directions:
+                    nexti, nextj = i + r, j + c
+                    
+                    if 0 <= nexti < rows and 0 <= nextj < cols and grid[nexti][nextj] == 1:
+                        rotten += 1
+                        grid[nexti][nextj] = 2
+                        queue.append((nexti, nextj))
+                        
+            minutes += 1
+                        
+        return minutes if rotten == (rows * cols) - empty else -1
