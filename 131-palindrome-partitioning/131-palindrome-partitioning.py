13717a2947289d70ab271bb750d1567e200db20f
Rohan Jagtap <rohanj30@icloud.com>
2022-10-08 10:13:57 +0530

    Time: 2419 ms (5.02%), Space: 30.2 MB (59.69%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -11,35 +11,37 @@ class Solution:
             
             return True
         
-#         def backtrack(start, end):
-#             if start < end:
-#                 for i in range(start + 1, end + 1):
-#                     sub = s[start: i]
-#                     if palindrome(sub):
-#                         partition.append(sub)
-#                         backtrack(i, end)
-#                         partition.pop()
-#             else:
-#                 partitions.append(partition[:])
-
-#         partition = []
-#         partitions = []
-#         backtrack(0, len(s))
-        
-#         return partitions
-        
-        @lru_cache(maxsize=None)
-        def backtrack(start, end, partition):
+        # O(n2n), O(n), backtracking
+        def backtrack(start, end):
             if start < end:
-                
-                partitions = []
                 for i in range(start + 1, end + 1):
                     sub = s[start: i]
                     if palindrome(sub):
-                        partitions.extend(backtrack(i, end, partition + (sub,)))
-                
-                return partitions
+                        partition.append(sub)
+                        backtrack(i, end)
+                        partition.pop()
             else:
-                return [list(partition)]
+                partitions.append(partition[:])
+
+        partition = []
+        partitions = []
+        backtrack(0, len(s))
+        
+        return partitions
+        
+#         O(n2n), O(n), backtracking + memoization
+#         @lru_cache(maxsize=None)
+#         def backtrack(start, end, partition):
+#             if start < end:
+                
+#                 partitions = []
+#                 for i in range(start + 1, end + 1):
+#                     sub = s[start: i]
+#                     if palindrome(sub):
+#                         partitions.extend(backtrack(i, end, partition + (sub,)))
+                
+#                 return partitions
+#             else:
+#                 return [list(partition)]
                          
-        return backtrack(0, len(s), ())
+#         return backtrack(0, len(s), ())
