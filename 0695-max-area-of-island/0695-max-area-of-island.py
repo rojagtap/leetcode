23ada89c698f7fe62638f1c5264a683b2f538e9c
Rohan Jagtap <rohanj30@icloud.com>
2022-10-10 21:11:02 +0530

    Time: 123 ms (99.24%), Space: 14.1 MB (95.66%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -13,15 +13,43 @@ class Solution:
                     
                     land = 0
                     while queue:
-                        land += 1
-
-                        r, c = queue.popleft()
-                        for x, y in directions:
-                            nextr, nextc = r + x, c + y
-                            if 0 <= nextr < rows and 0 <= nextc < cols and grid[nextr][nextc]:
-                                grid[nextr][nextc] = 0
-                                queue.append((nextr, nextc))
+                        for _ in range(len(queue)):
+                            land += 1
+                            
+                            r, c = queue.popleft()
+                            for x, y in directions:
+                                nextr, nextc = r + x, c + y
+                                if 0 <= nextr < rows and 0 <= nextc < cols and grid[nextr][nextc]:
+                                    grid[nextr][nextc] = 0
+                                    queue.append((nextr, nextc))
                                     
                     island = max(island, land)
                     
         return island
+
+#     O(mn), O(mn), bfs
+#     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+#         rows, cols = len(grid), len(grid[0])
+#         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        
+#         island = 0
+#         for i in range(rows):
+#             for j in range(cols):
+#                 if grid[i][j]:
+#                     grid[i][j] = 0
+#                     queue = deque([(i, j)])
+                    
+#                     land = 0
+#                     while queue:
+#                         land += 1
+
+#                         r, c = queue.popleft()
+#                         for x, y in directions:
+#                             nextr, nextc = r + x, c + y
+#                             if 0 <= nextr < rows and 0 <= nextc < cols and grid[nextr][nextc]:
+#                                 grid[nextr][nextc] = 0
+#                                 queue.append((nextr, nextc))
+                                    
+#                     island = max(island, land)
+                    
+#         return island
