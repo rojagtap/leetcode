deb7e20f0ef7ae6512a7bd462368521b7dc6ef9b
Rohan Jagtap <rohanj30@icloud.com>
2022-10-12 14:43:48 +0530

    Time: 1079 ms (70.40%), Space: 18.9 MB (62.42%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,15 @@
+class Solution:
+    # O(n), O(n)
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        indegree = defaultdict(int)
+        outdegree = defaultdict(int)
+        
+        for a, b in trust:
+            outdegree[a] += 1
+            indegree[b] += 1
+            
+        for i in range(1, n + 1):
+            if indegree[i] == n - 1 and outdegree[i] == 0:
+                return i
+            
+        return -1
