3439a64f0014b0a7373b104251910cd892d91f4e
Rohan Jagtap <rohanj30@icloud.com>
2022-10-12 14:25:19 +0530

    Time: 2354 ms (34.27%), Space: 14.2 MB (59.49%) - LeetHub

--- 0277-find-the-celebrity/0277-find-the-celebrity.py
+++ 0277-find-the-celebrity/0277-find-the-celebrity.py
@@ -2,6 +2,23 @@
 # return a bool, whether a knows b
 # def knows(a: int, b: int) -> bool:
 
+'''
+1. Start from 0 and check if it 'knows' anyone from 1 to n. If yes, then it means it cannot be a celebrity.
+2. Now start with the 'person' 0 'knows' and check if they 'know' the rest of the 'people'
+3. Finally, check if the 'person' who does not 'know' the rest of the 'people' is not 'known' by anyone and if 'they' don't 'know' anyone in a separate loop. If this is true, this 'person' is the celebrity
+
+Corner cases:
+
+Scenario 1: if 0 doesn't 'know' 1-4 and 'knows' 5, but 5 'knows' just 1 and no one else
+-> This won't be a problem because 5 'knows' 1 will be covered in the second iteration where we are checking both
+
+Scenario 2: if 0 doesn't 'know' 1-4 and 'know' 5, but 1 knows no one
+-> 1 might look like a potential candidate at first but 0 doesn't 'know' 1 so anyway it isn't a celebrity
+
+Scenario 3: After the first loop we find that 'n - 1' is the only candidate left, rest everyone 'knows' someone, even 'n - 1' knows say, 2 but that wasn't covered in the loop because we only look ahead.
+-> This will be same as Scenario 1, i.e., it will be covered in the second loop
+'''
+
 class Solution:
     # O(n), O(1)
     def findCelebrity(self, n: int) -> int:     
