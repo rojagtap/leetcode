bcaea1188ab36c23f2987c3608d80ed7c1efe7bd
Rohan Jagtap <rohanj30@icloud.com>
2022-10-13 13:38:58 +0530

    Time: 68 ms (17.38%), Space: 14.1 MB (23.74%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -2,16 +2,17 @@ class Solution:
     # O(2n), O(n), backtracking + memoization
     def rob(self, nums: List[int]) -> int:
         @lru_cache(maxsize=None)
-        def pick(money, start, end):
+        def pick(start, end):
             if start <= end:
+                money = nums[start]
                 if start == 0:
-                    maxmoney = pick(money + nums[start], start + 2, end - 1)
+                    maxmoney = money + pick(start + 2, end - 1)
                 else:
-                    maxmoney = pick(money + nums[start], start + 2, end)
+                    maxmoney = money + pick(start + 2, end)
 
-                return max(maxmoney, pick(money, start + 1, end))
+                return max(maxmoney, pick(start + 1, end))
             else:
-                return money
+                return 0
         
         
-        return pick(0, 0, len(nums) - 1)
+        return pick(0, len(nums) - 1)
