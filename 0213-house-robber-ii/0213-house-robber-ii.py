df6a9b74c6a607a825a9a6ff237fe61e4a6310db
Rohan Jagtap <rohanj30@icloud.com>
2022-10-13 13:34:38 +0530

    Time: 2883 ms (5.06%), Space: 472 MB (23.74%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -3,15 +3,15 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         @lru_cache(maxsize=None)
         def pick(money, start, end):
-            if start > end:
-                return money
-            else:
+            if start <= end:
                 if start == 0:
                     maxmoney = pick(money + nums[start], start + 2, end - 1)
                 else:
                     maxmoney = pick(money + nums[start], start + 2, end)
 
                 return max(maxmoney, pick(money, start + 1, end))
-                    
+            else:
+                return money
+        
         
         return pick(0, 0, len(nums) - 1)
