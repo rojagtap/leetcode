a9fa50edae6e605527aad172b3d56becaed983cd
Rohan Jagtap <rohanj30@icloud.com>
2022-10-13 14:07:44 +0530

    Time: 62 ms (35.20%), Space: 14 MB (23.74%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,10 +1,10 @@
 class Solution:
     # O(2n), O(n), backtracking + memoization
-    def rob(self, nums: List[int]) -> int:
+    def rob(self, houses: List[int]) -> int:
         @lru_cache(maxsize=None)
         def pick(start, end):
             if start <= end:
-                money = nums[start]
+                money = houses[start]
                 offset = 0
                 if start == 0:
                     offset = 1
@@ -12,4 +12,4 @@ class Solution:
             else:
                 return 0
         
-        return pick(0, len(nums) - 1)
+        return pick(0, len(houses) - 1)
