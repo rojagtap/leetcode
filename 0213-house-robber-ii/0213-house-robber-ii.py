7473cb4fa5590e636267393be40fc485e8761dff
Rohan Jagtap <rohanj30@icloud.com>
2022-10-13 14:08:25 +0530

    Time: 67 ms (20.05%), Space: 14.1 MB (23.74%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,5 +1,5 @@
 class Solution:
-    # O(2n), O(n), backtracking + memoization
+    # O(2^n), O(n), backtracking + memoization
     def rob(self, houses: List[int]) -> int:
         @lru_cache(maxsize=None)
         def pick(start, end):
