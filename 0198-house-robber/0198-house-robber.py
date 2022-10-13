5f92bfa8da20c7595cb93e3942e5dadf241acfbd
Rohan Jagtap <rohanj30@icloud.com>
2022-10-13 14:52:02 +0530

    Time: 50 ms (64.63%), Space: 14.2 MB (19.42%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,11 @@
+class Solution:
+    # O(2^n) ~ O(n), O(n), backtracking + memoization
+    def rob(self, houses: List[int]) -> int:
+        @lru_cache(maxsize=None)
+        def pick(start, end):
+            if start <= end:
+                return max(houses[start] + pick(start + 2, end), pick(start + 1, end))
+            else:
+                return 0
+        
+        return pick(0, len(houses) - 1)
