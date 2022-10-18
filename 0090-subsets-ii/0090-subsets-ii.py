1603ebc6ceffabeb3b95ecbd76dbe260479701dc
Rohan Jagtap <rohanj30@icloud.com>
2022-10-18 21:02:56 +0530

    Time: 86 ms (17.22%), Space: 14.1 MB (49.57%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        
+        subsets, start, end = [[]], 0, 0
+        for i in range(len(nums)):
+            if i == 0 or nums[i] != nums[i - 1]:
+                start = 0
+            else:
+                start = end
+                
+            end = len(subsets)
+            for j in range(start, end):
+                subsets.append(subsets[j] + [nums[i]])
+            
+        return subsets
