3127ad06e0c57bbd22115e32ee69f3453df6afd8
Rohan Jagtap <rohanj30@icloud.com>
2022-10-18 21:27:36 +0530

    Time: 121 ms (41.59%), Space: 17.9 MB (5.74%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -1,6 +1,7 @@
 class Solution:
     # O(2n), O(2n)
     def letterCasePermutation(self, s: str) -> List[str]:
+        @lru_cache(maxsize=None)
         def permutate(curr):
             if not rem:
                 permutations.append(curr)
