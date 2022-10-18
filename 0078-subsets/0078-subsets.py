acae42c15f20e1ba01158d7ae3e413f38dbd1d59
Rohan Jagtap <rohanj30@icloud.com>
2022-10-18 20:49:37 +0530

    Time: 66 ms (39.45%), Space: 14 MB (82.30%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,10 @@
+class Solution:
+    # O(2n), O(2n)
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subsets = [[]]
+        
+        for num in nums:
+            for i in range(len(subsets)):
+                subsets.append(subsets[i] + [num])
+                
+        return subsets
