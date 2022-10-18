13d648194894aeae908c1bb35159bba557032769
Rohan Jagtap <rohanj30@icloud.com>
2022-10-18 22:38:01 +0530

    Time: 469 ms (23.83%), Space: 21.5 MB (25.88%) - LeetHub

---
+++ 0320-generalized-abbreviation/0320-generalized-abbreviation.py
@@ -0,0 +1,22 @@
+class Solution:
+    # O(2n), O(2n)
+    def generateAbbreviations(self, word: str) -> List[str]:
+        def permutate(abbr, idx, count):
+            if idx == len(word):
+                if count != 0:
+                    abbr += str(count)
+                
+                abbrevations.append(abbr)
+            else:
+                permutate(abbr, idx + 1, count + 1)
+                
+                if count != 0:
+                    abbr += str(count)
+                    
+                abbr += word[idx]
+                permutate(abbr, idx + 1, 0)
+                
+        abbrevations = []
+        permutate("", 0, 0)
+        
+        return abbrevations
