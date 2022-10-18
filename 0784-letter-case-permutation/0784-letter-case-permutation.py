b209b59f652a6cc6632f01ed3e509cc2733d13cb
Rohan Jagtap <rohanj30@icloud.com>
2022-10-18 21:26:23 +0530

    Time: 131 ms (30.34%), Space: 15.4 MB (23.53%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        def permutate(curr):
+            if not rem:
+                permutations.append(curr)
+            else:
+                char = rem.popleft()
+                permutate(curr + char)
+
+                if char.isalpha():
+                    permutate(curr + char.swapcase())
+                
+                rem.appendleft(char)
+        
+        permutations = []
+        rem = deque(s)
+        
+        permutate("")
+        
+        return permutations
