2ec985124819e3a64814123099f1629b1a281b81
Rohan Jagtap <rohanj30@icloud.com>
2022-10-18 23:18:21 +0530

    Time: 38 ms (82.19%), Space: 13.9 MB (61.96%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -0,0 +1,21 @@
+class Solution:
+    # O(n2), O(n)
+    def numTrees(self, n: int) -> int:
+        @cache
+        def permutate(start, end):
+            if start >= end:
+                return 0
+            elif start == end - 1:
+                return 1
+            
+            trees = 0
+            for i in range(start, end):
+                left_trees = permutate(start, i) or 1
+                right_trees = permutate(i + 1, end) or 1
+                
+                trees += (left_trees * right_trees)
+                      
+            return trees
+        
+        return permutate(1, n + 1)
+                
