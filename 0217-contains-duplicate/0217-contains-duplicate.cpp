7371cb957d998b8018418d634f9df80e48c8a652
Rohan Jagtap <rohanj30@icloud.com>
2023-09-08 02:07:53 +0530

    Time: 151 ms (20.55%), Space: 73.5 MB (8.47%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+/*
+insert all elements in set
+compare set and list size
+*/
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> unique;
+
+        for (auto num : nums) {
+            unique.insert(num);
+        }
+
+        return nums.size() != unique.size();
+    }
+};
