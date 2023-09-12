24a9f62014bf90da9c2816103af42800c30f87bd
Rohan Jagtap <rohanj30@icloud.com>
2023-09-13 01:47:14 +0530

    Time: 7 ms (91.27%), Space: 10.9 MB (35.10%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,3 +1,10 @@
+/*
+create a reverse map of num -> index
+num1 + numx = target is the condition
+so num1 = target - numx
+therefore if target - numx is in the map,
+then we can return the current index and numx's index
+*/
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
