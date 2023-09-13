f10c1e15e3497cd1e092c5a765d88a3c1765f4c0
Rohan Jagtap <rohanj30@icloud.com>
2023-09-13 08:29:02 +0530

    Time: 9 ms (86.40%), Space: 15.4 MB (10.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,8 +1,12 @@
 /*
-create a frequency table of nums
-insert (frequency, number) in a max heap, ordered by frequency
-pop top k elements
-complexity will be O(klogn)
+create a count table of all elements in nums
+reverse the count table by creating a vector<vector<int>> of size equal to nums.size()
+the size of count table will never exceed nums.size()
+for each count, append the element of nums having that count to the inner vector
+traverse the reverse vector from the back (since we want top k) and fill a result vector
+until size is >= k
+return the first k elements
+complexity will be O(n)
 */
 class Solution {
 public:
@@ -12,17 +16,47 @@ public:
             ++freq[num];
         }
         
-        priority_queue<pair<int, int>> heapq;
+        vector<vector<int>> reverse(nums.size() + 1);
         for (auto it : freq) {
-            heapq.push(make_pair(it.second, it.first));
+            reverse[it.second].push_back(it.first);
         }
         
         vector<int> topk;
-        for (int i = 0; i < k; ++i) {
-            topk.push_back(heapq.top().second);
-            heapq.pop();
+        int i = nums.size();
+        while (i >= 0 && topk.size() < k) {
+            topk.insert(topk.end(), reverse[i].begin(), reverse[i].end());
+            --i;
         }
         
         return topk;
     }
 };
+
+// /*
+// create a frequency table of nums
+// insert (frequency, number) in a max heap, ordered by frequency
+// pop top k elements
+// complexity will be O(klogn)
+// */
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int, int> freq;
+//         for (auto num : nums) {
+//             ++freq[num];
+//         }
+        
+//         priority_queue<pair<int, int>> heapq;
+//         for (auto it : freq) {
+//             heapq.push(make_pair(it.second, it.first));
+//         }
+        
+//         vector<int> topk;
+//         for (int i = 0; i < k; ++i) {
+//             topk.push_back(heapq.top().second);
+//             heapq.pop();
+//         }
+        
+//         return topk;
+//     }
+// };
