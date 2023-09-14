faf1e15ca32eba60181c67400136f25bd2279403
Rohan Jagtap <rohanj30@icloud.com>
2023-09-15 00:48:59 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (12.71%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> parantheses;
+        unordered_map<char, char> pairing = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+        for (auto c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                parantheses.push(c);
+            } else if (!parantheses.empty() && parantheses.top() == pairing[c]) {
+                parantheses.pop();
+            } else {
+                return false;
+            }
+        }
+        
+        return parantheses.empty();
+    }
+};
