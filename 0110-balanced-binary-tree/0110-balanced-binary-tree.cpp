6b45ec7510ae79d37155f03c94d3f882deda8a25
Rohan Jagtap <rohanj30@icloud.com>
2023-09-27 20:30:11 +0530

    Time: 13 ms (29.98%), Space: 22.3 MB (5.05%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,41 +1,67 @@
 /*
-for all nodes do abs(left height - right height) and check if diff > 1
-also return alongside isbalanced(left) && isbalanced(right) && isbalanced(this)
+keep send pointer to height down the tree and update it at the end
+this way we dont have to re calculate or cache heights for each level
 */
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        if (!root) return true;
-
-        int left = get_height(root->left);
-        int right = get_height(root->right);
-
-        return isBalanced(root->left) && isBalanced(root->right) && abs(left - right) <= 1;
+        int height = 0;
+        return isBalanced(root, height);
     }
-    
-private:
-    unordered_map<TreeNode*, int> cache;
-    
-    int get_height(TreeNode* root) {
-        if (cache.find(root) != cache.end()) return cache.at(root);
 
-        int result;
-        if (!root) result = 0;
-        else result = max(get_height(root->left), get_height(root->right)) + 1;
+    bool isBalanced(TreeNode* root, int& height) {
+        if (!root) {
+            height = 0;
+            return true;
+        }
         
-        cache[root] = result;
-        return result;
+        int left = 0, right = 0;
+        bool balanced = isBalanced(root->left, left) && isBalanced(root->right, right) && abs(right - left) <= 1;
+        
+        height = max(left, right) + 1;
+        
+        return balanced;
     }
 };
+
+// /*
+// for all nodes do abs(left height - right height) and check if diff > 1
+// also return alongside isbalanced(left) && isbalanced(right) && isbalanced(this)
+// */
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     bool isBalanced(TreeNode* root) {
+//         if (!root) return true;
+
+//         int left = get_height(root->left);
+//         int right = get_height(root->right);
+
+//         return isBalanced(root->left) && isBalanced(root->right) && abs(left - right) <= 1;
+//     }
+    
+// private:
+//     unordered_map<TreeNode*, int> cache;
+    
+//     int get_height(TreeNode* root) {
+//         if (cache.find(root) != cache.end()) return cache.at(root);
+
+//         int result;
+//         if (!root) result = 0;
+//         else result = max(get_height(root->left), get_height(root->right)) + 1;
+        
+//         cache[root] = result;
+//         return result;
+//     }
+// };
