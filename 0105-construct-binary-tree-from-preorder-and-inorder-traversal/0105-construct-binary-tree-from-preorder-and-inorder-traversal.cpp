002b13532b63eb4a2468b7dd31f3205c8b1c49de
Rohan Jagtap <rohanj30@icloud.com>
2023-09-29 09:04:34 +0530

    Time: 12 ms (76.86%), Space: 26.7 MB (18.16%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,5 +1,16 @@
 /*
+preorder will always give the root of a subtree
+i.e., preorder[0] will always be the root; preorder[1] will be the root of left subtree if exists or else right, and so on
+inorder will tell the position of the subtree given by inorder, i.e., if preorder[0] is inorder[2], then inorder[0-2] will be the left subtree and inorder[3-n] will be the right subtree and as mentioned earlier, the roots will be given by preorder as preorder[1] will be left subtree root and preorder[2] will be right subtree root
 
+preorderindex: root index of the next subtree
+leftbound: index of inorder indicating the left bound of the current subtree, i.e., the current subtree cannot go outside this range
+rightbound: index of inorder indicating the right bound of the current subtree, i.e., the current subtree cannot go outside this range
+
+1. start with the root (preorder[0]) and find its index in inorder
+   everything to the left in inorder is the left subtree and vice versa for right
+2. we can thus use the inorder index obtained in 1 for getting the range of left subtree and right subtree
+3. now recursively. take preorder[1] as the new root but with right limit as inorderindex - 1
 */
 
 /**
