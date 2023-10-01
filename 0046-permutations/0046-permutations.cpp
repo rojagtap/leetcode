d061c536108415fa7359a64fa668220c98b70357
Rohan Jagtap <rohanj30@icloud.com>
2023-10-02 01:57:32 +0530

    Time: 5 ms (28.93%), Space: 9.9 MB (5.07%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,3 +1,14 @@
+/*
+each perm will be of size nums.size()
+so at each position from 0 to nums.size() we try each of the element from nums once
+
+one way to do that is to
+take nums as a set of available elements
+iterate all nums one by one
+in each iteration remove that element from nums and insert it to perm
+now we can recursively iterate through the remaining of nums to try various orders on remaining length of perm
+see code for better understanding
+*/
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
