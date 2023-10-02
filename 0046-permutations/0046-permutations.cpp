ebf9321bf9f6ab07c9c3ed1581c2ec6f4e94603c
Rohan Jagtap <rohanj30@icloud.com>
2023-10-02 09:31:28 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (19.66%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,42 +1,75 @@
 /*
-each perm will be of size nums.size()
-so at each position from 0 to nums.size() we try each of the element from nums once
+another way to solve this is to take a pivot for each position and try all the elements at that position
+recursively do this for each position
+so first we will try all for last position
+then second to last and so on...
 
-one way to do that is to
-take nums as a set of available elements
-iterate all nums one by one
-in each iteration remove that element from nums and insert it to perm
-now we can recursively iterate through the remaining of nums to try various orders on remaining length of perm
-see code for better understanding
+see the code for better understanding
 */
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        unordered_set<int> numset (nums.begin(), nums.end());
-        
-        vector<int> permutation;
-        recurse (numset, permutation);
+        recurse (nums, 0);
         return permutations;
     }
 
 private:
     vector<vector<int>> permutations;
     
-    void recurse (unordered_set<int>& nums, vector<int>& permutation) {
-        if (nums.size() == 0) {
-            permutations.push_back(vector<int>(permutation.begin(), permutation.end()));
+    void recurse (vector<int>& nums, int startindex) {
+        if (startindex == nums.size()) {
+            permutations.push_back(nums);
             return;
         }
         
-        unordered_set<int> numscopy(nums.begin(), nums.end());
-        for (auto num : numscopy) {
-            nums.erase(num);
-            permutation.push_back(num);
-            
-            recurse (nums, permutation);
-            
-            permutation.pop_back();
-            nums.insert(num);
+        for (int i = startindex; i < nums.size(); ++i) {
+            swap(nums[i], nums[startindex]);
+            recurse (nums, startindex + 1);
+            swap(nums[i], nums[startindex]);
         }
     }
+
 };
+
+// /*
+// each perm will be of size nums.size()
+// so at each position from 0 to nums.size() we try each of the element from nums once
+
+// one way to do that is to
+// take nums as a set of available elements
+// iterate all nums one by one
+// in each iteration remove that element from nums and insert it to perm
+// now we can recursively iterate through the remaining of nums to try various orders on remaining length of perm
+// see code for better understanding
+// */
+// class Solution {
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         unordered_set<int> numset (nums.begin(), nums.end());
+        
+//         vector<int> permutation;
+//         recurse (numset, permutation);
+//         return permutations;
+//     }
+
+// private:
+//     vector<vector<int>> permutations;
+    
+//     void recurse (unordered_set<int>& nums, vector<int>& permutation) {
+//         if (nums.size() == 0) {
+//             permutations.push_back(vector<int>(permutation.begin(), permutation.end()));
+//             return;
+//         }
+        
+//         unordered_set<int> numscopy(nums.begin(), nums.end());
+//         for (auto num : numscopy) {
+//             nums.erase(num);
+//             permutation.push_back(num);
+            
+//             recurse (nums, permutation);
+            
+//             permutation.pop_back();
+//             nums.insert(num);
+//         }
+//     }
+// };
