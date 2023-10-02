61b94da125604357c7b1db2bedfa1a0135006058
Rohan Jagtap <rohanj30@icloud.com>
2023-10-02 23:17:16 +0530

    Time: 11 ms (28.49%), Space: 8.7 MB (27.43%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,53 @@
+/*
+for each row, start from 0 to board.size
+see which col cannot be captured from 0 to board.size try that col
+recursively try the next row
+note that since we want all solutions we might need to start at row > 0
+so the next row needs to be (row + 1) % n
+*/
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        int qcount = 0;
+        vector<string> board(n, string(n, '.'));
+        
+        recurse(board, qcount, 0);
+        
+        return solutions;
+    }
+
+private:
+    vector<vector<string>> solutions;    
+    set<pair<int, int>> visitedboxes;
+    
+    void recurse (vector<string>& board, int& qcount, int i) {
+        if (qcount == board.size()) {
+            solutions.push_back(board);
+            return;
+        }
+        
+        for (int j = 0; j < board.size(); ++j) {
+            bool skip = false;
+            for (auto& box : visitedboxes) {
+                if (i == box.first || j == box.second || abs(box.first - i) == abs(box.second - j)) {
+                    skip = true;
+                    break;
+                }
+            }
+            
+            if (!skip) {
+                pair<int, int> index = {i, j};
+                
+                board[i][j] = 'Q';
+                visitedboxes.insert(index);
+                
+                ++qcount;
+                recurse(board, qcount, (i + 1) % board.size());
+                --qcount;
+
+                visitedboxes.erase(index);
+                board[i][j] = '.';
+            }
+        }
+    }
+};
