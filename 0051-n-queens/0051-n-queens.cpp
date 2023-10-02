52de3a9efe851b2042de6f49d158b3305f687704
Rohan Jagtap <rohanj30@icloud.com>
2023-10-02 23:24:57 +0530

    Time: 11 ms (28.49%), Space: 8.7 MB (27.43%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -10,16 +10,16 @@ public:
     vector<vector<string>> solveNQueens(int n) {
         int qcount = 0;
         vector<string> board(n, string(n, '.'));
-        
+
         recurse(board, qcount, 0);
-        
+
         return solutions;
     }
 
 private:
     vector<vector<string>> solutions;    
     set<pair<int, int>> visitedboxes;
-    
+
     bool captures (int i, int j) {
         for (auto& box : visitedboxes) {
             if (i == box.first || j == box.second || abs(box.first - i) == abs(box.second - j)) {
@@ -29,20 +29,20 @@ private:
 
         return false;
     }
-    
+
     void recurse (vector<string>& board, int& qcount, int i) {
         if (qcount == board.size()) {
             solutions.push_back(board);
             return;
         }
-        
+
         for (int j = 0; j < board.size(); ++j) {
             if (!captures(i, j)) {
                 pair<int, int> index = {i, j};
-                
+
                 board[i][j] = 'Q';
                 visitedboxes.insert(index);
-                
+
                 ++qcount;
                 recurse(board, qcount, (i + 1) % board.size());
                 --qcount;
