24d4640e855ea3c4c30ea4aecbd7f4b9c61a053e
Rohan Jagtap <rohanj30@icloud.com>
2023-10-03 08:35:50 +0530

    Time: 27 ms (75.35%), Space: 20.4 MB (10.63%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,34 @@
+/*
+default is maxheap so create a minheap using -ve values
+add to heap such that if size == k + 1, then pop the smallest element
+this way the top of heap is always the kth largest element
+overall complexity is logk
+*/
+class KthLargest {
+public:
+    KthLargest(int _k, vector<int>& nums) {
+        k = _k;
+        for (auto& num : nums) {
+            add(num);
+        }
+    }
+    
+    int add(int val) {
+        topk.push(-val);
+        if (topk.size() == k + 1) {
+            topk.pop();
+        }
+        
+        return -topk.top();
+    }
+    
+private:
+    int k;
+    priority_queue<int> topk;
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
