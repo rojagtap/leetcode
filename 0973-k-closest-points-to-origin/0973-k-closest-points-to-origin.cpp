cc8502772f8bd3e26d047fc18adee1b3560de756
Rohan Jagtap <rohanj30@icloud.com>
2023-10-03 09:00:47 +0530

    Time: 252 ms (42.75%), Space: 76.5 MB (33.66%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -0,0 +1,32 @@
+/*
+min heap with comparator comparing distance from origin
+*/
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        for (auto& point : points) {
+            closest.push(point);
+        }
+        
+        vector<vector<int>> kclosest;
+        while (k) {
+            kclosest.push_back(closest.top());
+            --k; closest.pop();
+        }
+        
+        return kclosest;
+    }
+    
+private:
+    class compare {
+        public:
+        bool operator() (vector<int>& pointa, vector<int>& pointb) {
+            int dista = pointa[0] * pointa[0] + pointa[1] * pointa[1];
+            int distb = pointb[0] * pointb[0] + pointb[1] * pointb[1];
+            
+            return dista > distb;
+        }
+    };
+    
+    priority_queue<vector<int>, vector<vector<int>>, compare> closest;
+};
