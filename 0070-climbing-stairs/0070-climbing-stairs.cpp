35e6c50c88c5b3ec3e5624e165b0d5a80ee41f3a
Rohan Jagtap <rohanj30@icloud.com>
2023-10-05 23:14:14 +0530

    Time: 3 ms (9.99%), Space: 6.4 MB (25.23%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -16,20 +16,40 @@ basically, nways so far + nways of the last odd/even n
 and nways(1) = 1, nways(2) = 2
 */
 
-// using recursion
+// iterative
 class Solution {
 public:
     int climbStairs(int n) {
-        if (cache.find(n) != cache.end()) return cache[n];
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-
-        int result = climbStairs(n - 1) + climbStairs(n - 2);
-        cache[n] = result;
-
-        return result;
+        // vector<int> dp(n + 1);
+        // dp[1] = 1;
+        // dp[2] = 2;
+        
+        vector<int> dp;
+        dp.push_back(0);
+        dp.push_back(1);
+        dp.push_back(2);
+        for (int i = 3; i <= n; ++i) {
+            dp.push_back(dp[i - 1] + dp[i - 2]);
+        }
+        
+        return dp[n];
     }
-
-private:
-    unordered_map<int, int> cache;
 };
+
+// // recursive
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if (cache.find(n) != cache.end()) return cache[n];
+//         if (n == 1) return 1;
+//         if (n == 2) return 2;
+
+//         int result = climbStairs(n - 1) + climbStairs(n - 2);
+//         cache[n] = result;
+
+//         return result;
+//     }
+
+// private:
+//     unordered_map<int, int> cache;
+// };
