efe550d3b38c9b8489f729ab86c11fcdd383660b
Rohan Jagtap <rohanj30@icloud.com>
2023-10-09 00:54:25 +0530

    Time: 16 ms (46.84%), Space: 16.8 MB (56.88%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,69 @@
+/*
+we can do a pseudo-bfs
+start from n=end and look for all i that reach n take the min of them as it is closest to 0
+then search for all the values that go until >= closest and take its min
+repeat until n is 0
+
+this can be O(n^2) as every time we search from 0 to n (for example [1,1,1,1,1,1,1,1])
+but if we do the same thing in forward direction, it will be O(n)
+
+start from 0
+0 can go from 1 till nums[0]
+so go from 1 to nums[0] and take the max reach, i.e., it will give the max this set can reach
+now go from nums[0] + 1 to max and repeat
+go until nums.size is the max or greater than that
+
+we are pretty much doing the same thing in both approaches, just that since the direction of jumps is forward,
+we can use that to search for the next level (in bfs) in linear time
+
+this can be also solved using dp
+*/
+
+// forward bfs O(n)
 class Solution {
 public:
     int jump(vector<int>& nums) {
         int jumps = 0;
-        int n = nums.size() - 1;
 
-        int goal = n;
-        while (goal) {
-            int mingoal = INT_MAX;
-            for (int i = 0; i < n; ++i) {
-                if (i + nums[i] >= goal) {
-                    mingoal = min(mingoal, i);
-                }
+        int l = 0, r = 0;
+        while (r < nums.size()) {
+            if (r >= nums.size() - 1) {
+                break;
             }
-            goal = mingoal;
+
+            int maxjump = INT_MIN;
+            for (int j = l; j <= r; ++j) {
+                maxjump = max(maxjump, j + nums[j]);
+            }
+
+            l = r + 1;
+            r = maxjump;
             ++jumps;
         }
 
         return jumps;
     }
 };
+
+// reverse bfs O(n^2)
+// class Solution {
+// public:
+//     int jump(vector<int>& nums) {
+//         int jumps = 0;
+//         int n = nums.size() - 1;
+
+//         int goal = n;
+//         while (goal) {
+//             int mingoal = INT_MAX;
+//             for (int i = 0; i < n; ++i) {
+//                 if (i + nums[i] >= goal) {
+//                     mingoal = min(mingoal, i);
+//                 }
+//             }
+//             goal = mingoal;
+//             ++jumps;
+//         }
+
+//         return jumps;
+//     }
+// };
