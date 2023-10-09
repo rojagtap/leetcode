594aa03693be6806269266a3ff01731c9e70ab7a
Rohan Jagtap <rohanj30@icloud.com>
2023-10-10 00:41:42 +0530

    Time: 102 ms (27.62%), Space: 16 MB (47.89%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -17,20 +17,50 @@ amount = 11, count = 3
 so basically dp[amount] = 1 + min(dp[amount - 1], dp[amount - 2], dp[amount - 5])
 i.e., if amount - c is seen already then use its minvalue + 1
 */
+
+// top-down
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, amount + 1);
-        dp[0] = 0;
+        dp = vector<int>(amount + 1);
+        change(coins, amount);
+        return dp[amount] == dp.size() ? -1 : dp[amount];
+    }
 
-        for (int a = 1; a <= amount; ++a) {
-            for (auto& coin : coins) {
-                if (a - coin >= 0) {
-                    dp[a] = min(dp[a], 1 + dp[a - coin]);
-                }
+private:
+    int MAX;
+    vector<int> dp;
+
+    int change(vector<int>& coins, int amount) {
+        if (amount == 0) return 0;
+        if (dp[amount] != 0) return dp[amount];
+
+        dp[amount] = dp.size();
+        for (int i = 0; i < coins.size(); ++i) {
+            if (coins[i] <= amount) {
+                dp[amount] = min(dp[amount], 1 + change(coins, amount - coins[i]));
             }
         }
 
-        return dp.back() == amount + 1 ? -1 : dp.back();
+        return dp[amount];
     }
 };
+
+// // bottom-up
+// class Solution {
+// public:
+//     int coinChange(vector<int>& coins, int amount) {
+//         vector<int> dp(amount + 1, amount + 1);
+//         dp[0] = 0;
+
+//         for (int a = 1; a <= amount; ++a) {
+//             for (auto& coin : coins) {
+//                 if (a - coin >= 0) {
+//                     dp[a] = min(dp[a], 1 + dp[a - coin]);
+//                 }
+//             }
+//         }
+
+//         return dp.back() == amount + 1 ? -1 : dp.back();
+//     }
+// };
