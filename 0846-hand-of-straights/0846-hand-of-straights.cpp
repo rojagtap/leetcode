56f23398959ad799f3f35e76bd3942f32880f283
Rohan Jagtap <rohanj30@icloud.com>
2023-10-09 11:12:28 +0530

    Time: 78 ms (18.25%), Space: 36.1 MB (5.28%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -0,0 +1,71 @@
+/*
+for hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
+
+sort = [1,2,2,3,3,4,6,7,8]
+reverse = {
+    1: [0],
+    2: [1, 2],
+    3: [3, 4],
+    4: [5],
+    6: [6],
+    7: [7],
+    8: [8]
+}
+
+i = 0:
+    n = 1:
+    back = 2
+    sort = [1,2,-1,3,3,4,6,7,8]
+    n = 2:
+    back = 4
+    sort = [1,2,-1,3,-1,4,6,7,8]
+i = 1:
+    n = 1:
+    back = 3
+    sort = [1,2,-1,-1,-1,4,6,7,8]
+    n = 2:
+    back = 5
+    sort = [1,2,-1,-1,-1,-1,6,7,8]
+i = 2:
+    skip
+...
+...
+i = 6:
+    n = 1:
+    back = 7
+    sort = [1,2,-1,-1,-1,-1,6,-1,8]
+    n = 2:
+    back = 8
+    sort = [1,2,-1,-1,-1,-1,6,-1,-1]
+*/
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize != 0) return false;
+
+        sort(hand.begin(), hand.end());
+
+        unordered_map<int, vector<int>> reverse;
+        for (int i = 0; i < hand.size(); ++i) {
+            reverse[hand[i]].push_back(i);
+        }
+
+        for (int i = 0; i < hand.size(); ++i) {
+            if (hand[i] == -1) continue;
+
+            int n = 1;
+            while(n < groupSize) {
+                if (reverse[hand[i] + n].empty()) {
+                    return false;
+                }
+                int back = reverse[hand[i] + n].back();
+                reverse[hand[i] + n].pop_back();
+
+                hand[back] = -1;
+                ++n;
+            }
+        }
+
+        return true;
+    }
+};
