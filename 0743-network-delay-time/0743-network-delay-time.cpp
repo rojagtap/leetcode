5554872e74280a00d571e7f97799743cc23075e0
Rohan Jagtap <rohanj30@icloud.com>
2023-10-14 03:56:56 +0530

    Time: 111 ms (24.23%), Space: 49.8 MB (9.98%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -5,40 +5,44 @@ if any node is not visited, return -1
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int, vector<vector<int>>> adjacency;
+        vector<vector<vector<int>>> adjacency(n + 1);
         for (auto& time : times) {
             adjacency[time[0]].push_back({time[1], time[2]});
         }
 
-        int visitedcount = 1;
-        vector<bool> visited(n + 1, false);
         vector<int> mintime(n + 1, INT_MAX);
-
-        mintime[0] = 0;
         mintime[k] = 0;
-        while (visitedcount < n) {
-            visited[k] = true;
+
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minnext;
+
+        minnext.push({0, k});
+        while (!minnext.empty()) {
+            k = minnext.top()[1];
+            int currtime = minnext.top()[0];
+            minnext.pop();
+
+            if (mintime[k] < currtime) {
+                // because heap has duplicates contrary to mintime which overwrites the shortest
+                // path for each vertex
+                // this condition means there is an old residual entry for the node k in heap
+                continue;
+            }
 
             for (auto& time : adjacency[k]) {
-                if (!visited[time[0]]) {
-                    mintime[time[0]] = min(mintime[time[0]], mintime[k] + time[1]);
+                // we don't need a visited marker because
+                // the basic assumption of dijkstra's is 
+                // if we traverse the shortest path from A -> D via B
+                // then B -> D will also be the shortest path from B
+                // hence it will never be possible that an already visited node
+                // will have a shorter path than existing
+                if (mintime[k] + time[1] < mintime[time[0]]) {
+                    mintime[time[0]] = mintime[k] + time[1];
+                    minnext.push({mintime[time[0]], time[0]});
                 }
             }
-
-            int nextk = -1;
-            int minnext = INT_MAX;
-            for (int i = 1; i <= n; ++i) {
-                if (!visited[i] && mintime[i] < minnext) {
-                    minnext = mintime[i];
-                    nextk = i;
-                }
-            }
-
-            if (nextk == -1) return -1;
-            k = nextk;
-            ++visitedcount;
         }
 
-        return *max_element(mintime.begin(), mintime.end());
+        int time = *max_element(mintime.begin() + 1, mintime.end());
+        return time == INT_MAX ? -1 : time;
     }
 };
