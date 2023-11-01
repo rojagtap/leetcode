414b219bb7cee05a8b49265b3ee04d9f8aaa61e3
Rohan Jagtap <rohanj30@icloud.com>
2023-11-01 21:13:42 +0530

    Time: 1919 ms (5.03%), Space: 18.7 MB (45.1%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,36 +2,60 @@
 this is a simple take/don't take problem
 */
 
-// backtracking + memoization, top-down dp
+// bottom-up dp
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        dp = vector<vector<int>>(coins.size(), vector<int>(amount + 1, -1));
-        return recurse(coins, amount, 0);
-    }
-
-private:
-    vector<vector<int>> dp;
-
-    int recurse(vector<int>& coins, int amount, int start) {
-        if (amount == 0) {
-            return 1;
+        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, 0));
+        for (int i = 0; i < coins.size(); ++i) {
+            dp[i][0] = 1;
         }
 
-        if (dp[start][amount] != -1) {
-            return dp[start][amount];
-        }
-
-        int ways = 0;
-        for (int i = start; i < coins.size(); ++i) {
-            if (amount - coins[i] >= 0) {
-                ways += recurse(coins, amount - coins[i], i);
+        for (int a = 1; a <= amount; ++a) {
+            for (int start = 0; start < coins.size(); ++start) {
+                for (int i = start; i < coins.size(); ++i) {
+                    if (a - coins[i] >= 0) {
+                        dp[start][a] += dp[i][a - coins[i]];
+                    }
+                }
             }
         }
 
-        return dp[start][amount] = ways;
+        return dp[0][amount];
     }
 };
+
+// // backtracking + memoization, top-down dp
+// // O(n^2 * amount) in time and space + O(amount) for recursion stack
+// class Solution {
+// public:
+//     int change(int amount, vector<int>& coins) {
+//         dp = vector<vector<int>>(coins.size(), vector<int>(amount + 1, -1));
+//         return recurse(coins, amount, 0);
+//     }
+
+// private:
+//     vector<vector<int>> dp;
+
+//     int recurse(vector<int>& coins, int amount, int start) {
+//         if (amount == 0) {
+//             return 1;
+//         }
+
+//         if (dp[start][amount] != -1) {
+//             return dp[start][amount];
+//         }
+
+//         int ways = 0;
+//         for (int i = start; i < coins.size(); ++i) {
+//             if (amount - coins[i] >= 0) {
+//                 ways += recurse(coins, amount - coins[i], i);
+//             }
+//         }
+
+//         return dp[start][amount] = ways;
+//     }
+// };
 
 // // backtracking
 // // O(2^n) in time and O(amount) in space for recursion stack
