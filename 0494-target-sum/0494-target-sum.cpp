3a65d10353a2051d76ae84ef93a4ea732750ac24
Rohan Jagtap <rohanj30@icloud.com>
2023-11-01 23:49:16 +0530

    Time: 293 ms (35.54%), Space: 62.8 MB (11.46%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -3,7 +3,7 @@ similar to take/don't take
 just one choice is '+' and other is '-'
 */
 
-// backtracking
+// backtracking + memoization, top-down dp
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -11,12 +11,36 @@ public:
     }
 
 private:
+    map<pair<int, int>, int> dp;
+
     int recurse(vector<int>& nums, int target, int i) {
         if (i == nums.size()) {
             return target == 0;
         }
     
+        if (dp.find({target, i}) != dp.end()) {
+            return dp[{target, i}];
+        }
+
         // take/don't take (- or +)
-        return recurse(nums, target - nums[i], i + 1) + recurse(nums, target + nums[i], i + 1);
+        return dp[{target, i}] = recurse(nums, target - nums[i], i + 1) + recurse(nums, target + nums[i], i + 1);
     }
 };
+
+// // backtracking, O(2^n) in time and O(n) in space (recursion stack)
+// class Solution {
+// public:
+//     int findTargetSumWays(vector<int>& nums, int target) {
+//         return recurse(nums, target, 0);
+//     }
+
+// private:
+//     int recurse(vector<int>& nums, int target, int i) {
+//         if (i == nums.size()) {
+//             return target == 0;
+//         }
+    
+//         // take/don't take (- or +)
+//         return recurse(nums, target - nums[i], i + 1) + recurse(nums, target + nums[i], i + 1);
+//     }
+// };
