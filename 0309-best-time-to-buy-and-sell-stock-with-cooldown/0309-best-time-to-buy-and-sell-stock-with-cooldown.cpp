f3407a3c94f2c2072b5be55e5de04d8f1ea1bcaf
Rohan Jagtap <rohanj30@icloud.com>
2023-11-01 19:34:13 +0530

    Time: 0 ms (100%), Space: 11.5 MB (86.94%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -10,21 +10,48 @@ pass on -price + profit for buy and price + profit for sell and just profit for 
 however, this doesn't work as this does not repeat. i.e., the same profit for a given buy/sell choice for an index won't repeat, only the buy/sell + index will. so we want to just obtain the profit of the function independent of the previous gain
 */
 
-// bottom-up dp, just translate top-down to table 
-// O(n) in time and space
+// bottom-up dp, but we can skip the array
+// as we only need 2 further values not the entire array
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(2, vector<int>(prices.size() + 2, 0));
+        // we just need these from the dp array
+        int buyplus1 = 0, buyplus2 = 0, sellplus1 = 0;
 
         for (int i = prices.size() - 1; i >= 0; --i) {
-            dp[0][i] = max(prices[i] + dp[1][i + 2], dp[0][i + 1]);
-            dp[1][i] = max(-prices[i] + dp[0][i + 1], dp[1][i + 1]);
+            // easier to read
+            // int sellprofit = max(prices[i] + buyplus2, sellplus1);
+            // int buyprofit = max(-prices[i] + sellplus1, buyplus1);
+
+            // buyplus2 = buyplus1;
+            // buyplus1 = buyprofit;
+            // sellplus1 = sellprofit;
+
+            int temp = buyplus1;
+            buyplus1 = max(-prices[i] + sellplus1, buyplus1);
+            sellplus1 = max(prices[i] + buyplus2, sellplus1);
+            buyplus2 = temp;
         }
 
-        return dp[1][0];
+        return buyplus1;
     }
 };
+
+// // bottom-up dp, just translate top-down to table 
+// // O(n) in time and space
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         vector<vector<int>> dp(2, vector<int>(prices.size() + 2, 0));
+
+//         for (int i = prices.size() - 1; i >= 0; --i) {
+//             dp[0][i] = max(prices[i] + dp[1][i + 2], dp[0][i + 1]);
+//             dp[1][i] = max(-prices[i] + dp[0][i + 1], dp[1][i + 1]);
+//         }
+
+//         return dp[1][0];
+//     }
+// };
 
 // // backtracking + memoization, top-down dp
 // // O(n) in time and space (2*n)
