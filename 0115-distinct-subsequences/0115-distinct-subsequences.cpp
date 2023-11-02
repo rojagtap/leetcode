95b19a9fd40552681f0d7139186ff9bbb503e6b5
Rohan Jagtap <rohanj30@icloud.com>
2023-11-03 00:19:24 +0530

    Time: 39 ms (40.91%), Space: 30.7 MB (21.9%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -3,7 +3,7 @@ backtracking with take/don't take
 forced take and don't take as we need to try forward as well
 */
 
-// backtracking + memoization, top-down dp
+// bottom-up dp, O(m * n) in time and space
 class Solution {
 public:
     int numDistinct(string& s, string& t) {
@@ -11,37 +11,65 @@ public:
             return 0;
         }
 
-        dp = vector<vector<int>>(s.size(), vector<int>(t.size(), -1));
-        return recurse(s, t, 0, 0);
-    }
-
-private:
-    vector<vector<int>> dp;
-    int recurse(string& s, string& t, int i, int j) {
-        if (j == t.size()) {
-            return 1;
+        vector<vector<unsigned long>> dp(s.size() + 1, vector<unsigned long>(t.size() + 1, 0));
+        for (auto& row : dp) {
+            row[t.size()] = 1;
         }
 
-        if (i == s.size()) {
-            return 0;
+        for (int i = s.size() - 1; i >= 0; --i) {
+            for (int j = t.size() - 1; j >= 0; --j) {
+                if (s[i] == t[j]) {
+                    dp[i][j] += dp[i + 1][j + 1];
+                }
+
+                dp[i][j] += dp[i + 1][j];
+            }
         }
 
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int ways = 0;
-        if (s[i] == t[j]) {
-            ways += recurse(s, t, i + 1, j + 1);
-        }
-
-        ways += recurse(s, t, i + 1, j);
-
-        return dp[i][j] = ways;
+        return dp[0][0];
     }
 };
 
-// // backtracking O(2^m) in time and O(m) in space
+// // backtracking + memoization, top-down dp
+// // O(m * n) in time and space + O(m) space for recursion stack
+// class Solution {
+// public:
+//     int numDistinct(string& s, string& t) {
+//         if (s.size() < t.size()) {
+//             return 0;
+//         }
+
+//         dp = vector<vector<int>>(s.size(), vector<int>(t.size(), -1));
+//         return recurse(s, t, 0, 0);
+//     }
+
+// private:
+//     vector<vector<int>> dp;
+//     int recurse(string& s, string& t, int i, int j) {
+//         if (j == t.size()) {
+//             return 1;
+//         }
+
+//         if (i == s.size()) {
+//             return 0;
+//         }
+
+//         if (dp[i][j] != -1) {
+//             return dp[i][j];
+//         }
+
+//         int ways = 0;
+//         if (s[i] == t[j]) {
+//             ways += recurse(s, t, i + 1, j + 1);
+//         }
+
+//         ways += recurse(s, t, i + 1, j);
+
+//         return dp[i][j] = ways;
+//     }
+// };
+
+// // backtracking O(2^m) in time and O(m) in space for recursion stack
 // // where m is the length of s
 // class Solution {
 // public:
