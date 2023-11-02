7f68fd1904d8173367f043524ac4cd1c232e4d78
Rohan Jagtap <rohanj30@icloud.com>
2023-11-02 21:43:53 +0530

    Time: 3 ms (75.25%), Space: 6.9 MB (71.58%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -4,11 +4,14 @@ basically try take from s1 if equal then take from s2 or else return false
 
 in top-down case, we are typically getting the result for each (i, j, k) pair
 but k = i + j, so we can condense it to (i, j) pair for memoization
+
+in bottom up, we can simply translate the top-down approach and traverse the dp array in reverse
+finally since we only need the ith and the (i + 1)th row, we can use a 1-D dp array instead
 */
 
-// bottom-up dp
-// translate top-down
-// O(m * n) in time and space
+// bottom-up dp with space optimization
+// we only need the ith and the (i + 1)th row, not the entire dp array
+// O(m * n) in time and O(n) in space
 // here m is s1.size() and n is s2.size()
 class Solution {
 public:
@@ -17,23 +20,56 @@ public:
             return false;
         }
 
-        vector<vector<bool>> dp(s1.size() + 1, vector<bool>(s2.size() + 1, false));
-        dp[s1.size()][s2.size()] = true;
+        vector<bool> curr(s2.size() + 1, false);
+        curr[s2.size()] = true;
+        vector<bool> next(s2.size() + 1, false);
+        next[s2.size()] = true;
 
         for (int i = s1.size(); i >= 0; --i) {
             for (int j = s2.size(); j >= 0; --j) {
-                if (i < s1.size() && s1[i] == s3[i + j] && dp[i + 1][j]) {
-                    dp[i][j] = true;
+                if (i < s1.size() && s1[i] == s3[i + j] && next[j]) {
+                    curr[j] = true;
                 }
-                if (j < s2.size() && s2[j] == s3[i + j] && dp[i][j + 1]) {
-                    dp[i][j] = true;
+                if (j < s2.size() && s2[j] == s3[i + j] && curr[j + 1]) {
+                    curr[j] = true;
                 }
             }
+            next = curr;
+            curr = vector<bool>(s2.size() + 1, false);
         }
 
-        return dp[0][0];
+        return next[0];
     }
 };
+
+// // bottom-up dp
+// // translate top-down
+// // O(m * n) in time and space
+// // here m is s1.size() and n is s2.size()
+// class Solution {
+// public:
+//     bool isInterleave(string& s1, string& s2, string& s3) {
+//         if (s1.size() + s2.size() != s3.size()) {
+//             return false;
+//         }
+
+//         vector<vector<bool>> dp(s1.size() + 1, vector<bool>(s2.size() + 1, false));
+//         dp[s1.size()][s2.size()] = true;
+
+//         for (int i = s1.size(); i >= 0; --i) {
+//             for (int j = s2.size(); j >= 0; --j) {
+//                 if (i < s1.size() && s1[i] == s3[i + j] && dp[i + 1][j]) {
+//                     dp[i][j] = true;
+//                 }
+//                 if (j < s2.size() && s2[j] == s3[i + j] && dp[i][j + 1]) {
+//                     dp[i][j] = true;
+//                 }
+//             }
+//         }
+
+//         return dp[0][0];
+//     }
+// };
 
 // // backtracking + memoization, top-down dp
 // // O(m * n) in time and space + O(m + n) space for recursion stack
