cceba411719684ee7baaeb7ac508a23751932713
Rohan Jagtap <rohanj30@icloud.com>
2023-11-03 19:43:34 +0530

    Time: 19 ms (26.44%), Space: 9.3 MB (57.8%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -13,52 +13,77 @@ else try one of each choices:
 note that each operation will add 1 to their result
 */
 
-// backtracking + memoization
-// O(m * n) in time and space + O(n) for recursion stack
+// bottom-up dp, translate top-down
+// O(m * n) in time and space
 // where m is word1.size() and n is word2.size()
 class Solution {
 public:
     int minDistance(string& word1, string& word2) {
-        dp = vector<vector<int>>(word1.size(), vector<int>(word2.size(), -1));
-        return recurse(word1, word2, 0, 0);
-    }
-
-private:
-    vector<vector<int>> dp;
-
-    int recurse(string& word1, string& word2, int i, int j) {
-        if (i == word1.size() && j == word2.size()) {
-            return 0;
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, 0));
+        for (int i = 0; i < word1.size(); ++i) {
+            dp[i][word2.size()] = word1.size() - i;
         }
 
-        if (j == word2.size()) {
-            // nothing left in word2 but word1 is still having chars
-            // so try deleting the rest
-            return word1.size() - i;
+        for (int j = 0; j < word2.size(); ++j) {
+            dp[word1.size()][j] = word2.size() - j;
+        }
+        
+        for (int i = word1.size() - 1; i >= 0; --i) {
+            for (int j = word2.size() - 1; j >= 0; --j) {
+                dp[i][j] = (word1[i] == word2[j]) ? dp[i + 1][j + 1] : 1 + min({dp[i][j + 1], dp[i + 1][j], dp[i + 1][j + 1]});
+            }
         }
 
-        if (i == word1.size()) {
-            // nothing left in word1 to replace or to delete
-            // so try inserting until there are still chars in word2
-            return word2.size() - j;
-        }
-
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        if (word1[i] == word2[j]) {
-            // equal case
-            return dp[i][j] = recurse(word1, word2, i + 1, j + 1);
-        } else {
-            return dp[i][j] = 1 + min({
-                recurse(word1, word2, i, j + 1),    // insert
-                recurse(word1, word2, i + 1, j),    // delete
-                recurse(word1, word2, i + 1, j + 1) // substitute
-            });
-        }
+        return dp[0][0];
     }
 };
+
+// // backtracking + memoization
+// // O(m * n) in time and space + O(n) for recursion stack
+// // where m is word1.size() and n is word2.size()
+// class Solution {
+// public:
+//     int minDistance(string& word1, string& word2) {
+//         dp = vector<vector<int>>(word1.size(), vector<int>(word2.size(), -1));
+//         return recurse(word1, word2, 0, 0);
+//     }
+
+// private:
+//     vector<vector<int>> dp;
+
+//     int recurse(string& word1, string& word2, int i, int j) {
+//         if (i == word1.size() && j == word2.size()) {
+//             return 0;
+//         }
+
+//         if (j == word2.size()) {
+//             // nothing left in word2 but word1 is still having chars
+//             // so try deleting the rest
+//             return word1.size() - i;
+//         }
+
+//         if (i == word1.size()) {
+//             // nothing left in word1 to replace or to delete
+//             // so try inserting until there are still chars in word2
+//             return word2.size() - j;
+//         }
+
+//         if (dp[i][j] != -1) {
+//             return dp[i][j];
+//         }
+
+//         if (word1[i] == word2[j]) {
+//             // equal case
+//             return dp[i][j] = recurse(word1, word2, i + 1, j + 1);
+//         } else {
+//             return dp[i][j] = 1 + min({
+//                 recurse(word1, word2, i, j + 1),    // insert
+//                 recurse(word1, word2, i + 1, j),    // delete
+//                 recurse(word1, word2, i + 1, j + 1) // substitute
+//             });
+//         }
+//     }
+// };
 
 // // backtracking
 // // O(3 ^ n) in time and O(n) in space (recursion stack), TLE
