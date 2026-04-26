1f87b32ee3c6404cf9d6743d85aa96083fa96ea9
Rohan Jagtap <rohanj30@icloud.com>
2026-04-26 16:06:01 +0530

    Time: 2900 ms (5.06%), Space: 315.6 MB (48.77%) - LeetHub

--- 3915-maximum-sum-of-alternating-subsequence-with-distance-at-least-k/3915-maximum-sum-of-alternating-subsequence-with-distance-at-least-k.cpp
+++ 3915-maximum-sum-of-alternating-subsequence-with-distance-at-least-k/3915-maximum-sum-of-alternating-subsequence-with-distance-at-least-k.cpp
@@ -1,3 +1,46 @@
+/**
+looking at the problem, i first thought about the bruteforce
+so the first thing is as simple as either you take the current num or you don't like a knapsack
+if you take the current value, you go +k, or else, you go +1.
+i also came to the realization that taking the kth value alone is not sufficient, we need to try
+for all j >= i + k. now this can be achieved in two ways:
+
+1. dfs(i, prev, up) -> max(for (j : i ... n) if (nums[i] > nums[prev]) dfs(i + k, nums[i], down))
+and dfs(i, prev, down) -> max(for (j : i ... n)) if (nums[i] < nums[prev]) dfs(i + k, nums[i], up))
+2. dfs(i, prev, up) -> max(if (nums[i] > prev) dfs(i + k, nums[i], down), dfs(i + 1, prev, up))
+and dfs(i, prev, down) -> max(if (nums[i] < prev) dfs(i + k, nums[i], up), dfs(i + 1, prev, down))
+(i just realized this one now)
+
+looking at this bruteforce, i concluded that there are two axes,
+- one is the "distance" (j >= i + k)
+- and the other is max value (not score)
+either of these cannot be picked greedily:
+- if you always pick i + k, there might be a better outcome at i + k + x
+- if you always pick the max value from i + k ... n, then you might miss out on two smaller
+values that add up > max value
+so basically, we need to check the max score we can get from all the positions from i + k ... n,
+for all the values that are greater than (for up, or smaller for down) nums[i]
+
+moreover, even with memoization and everything, this will still be O(n^2) i guess.
+looking at the constraints, that doesn't seem to be acceptable.
+
+now for the above two issues:
+- for the range, we can easily accumulate all the results from i + k ... n at i + k,
+so that we can just query some dp[up/down][i + k] that will give us the best result in i + k ... n
+- for the max value, we need to basically find the max "score" we can achieve for any number
+either from nums[i] + 1 to 1e5 for up, or 1 to nums[i] - 1 for down.
+note that initially, i thought we have to keep all the values seen so far from n till j = i + k
+in a sorted set so we can query the next greater value or the next smaller value than nums[i].
+but i was wrong. since we are going in reverse (like in any dp) i.e. we are iterating n ... j,
+we can simply try for nums[i] + 1 and - 1, because we only have results up till j.
+so it doesn't matter if you query the next greater number or just nums[i] + 1.
+
+all in all, we need to find a way to get the best score from j ... n,
+across all numbers > nums[i] for up and < nums[i] for down
+this is range query. we can use prefix/suffix arrays for 0 ... 1e5,
+but the range updates are dynamic, so this is an use case for either segment tree or fenwick trees.
+**/
+
 class SegTree {
 private:
     int n;
