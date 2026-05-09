daafeaef344acb53c1dc03460d6ee42610aa48bd
Rohan Jagtap <rohanj30@icloud.com>
2026-05-09 22:01:23 +0530

    Time: 51 ms (83.84%), Space: 181.3 MB (84.88%) - LeetHub

--- 3920-maximize-fixed-points-after-deletions/3920-maximize-fixed-points-after-deletions.cpp
+++ 3920-maximize-fixed-points-after-deletions/3920-maximize-fixed-points-after-deletions.cpp
@@ -1,3 +1,44 @@
+/**
+during the contest, initially i thought about bruteforce where you try all possible
+combinations of shifting numbers that can be shifted and count the max.
+there is a repeating pattern here. since the shifts go left, the max points from i to n
+don't change irrespective what you have until i for a given number of shifts until i
+i.e., we can take carry and i and either move the ith element to i - carry - nums[i] or skip
+carry is basically the number of elements deleted so far.
+after submitting, i realized that the above logic won't work for arrays with duplicates.
+so i tried taking a visited array to see if the a given position was already filled.
+the problem here is taking an entire visited state for 1e5 elements is not possible alongside
+carry and i. so i tried taking a separate visited array and a separate cache which won't work
+
+here's when the contest ended.
+
+now after the contest, i check other people's solutions + chatgpt and it was LIS
+but I was not sure how.
+
+the solutions had a tuple of (diff, nums[i])'s and finding the LIS for these. it sounded fine.
+but when thinking of this approach, i first thought it works because we always pick the smallest diff first since it has the least number of deletions. and from here, i spiraled:
+first of all, the LIS approach is not about picking diffs in increasing order. i mean it does that but the approach is not about doing this. it's not about the order of deletions.
+GPT's solution also included sorting by diff in ascending and nums by descending order which
+made me think picking rightmost elements first is better because they affect fewer items to the right. which again, is about order when it shouldn't be.
+second, this led me into thinking about all sorts of scenarios like:
+- what happens if i delete a number which could have been placed to it's spot?
+- assuming the diff and current position i as an interval, what if two intervals overlap?
+full overlaps are still ok, how do you handle partial overlaps? which one goes first? (again order)
+- what if the shortest diff interval overlaps with two such intervals where we could have picked both if not for the shortest?
+missing the fundamental LIS concept.
+after taking this up with chatgpt further,
+i realized that we can think about the problem like this:
+for any two numbers x (at i) and y (at j) where x < y, to be able to pick both,
+if i < j:
+then diffx <= diffy because y shifts more (once for x and then for itself)
+if i > j:
+then it's not possible to pick both
+so this tells me that to get multiple fixed points, i need to get points that are
+a. increasing in value
+b. increasing in diff
+that's LIS with one value sorted and other LIS.
+**/
+
 class Solution {
 public:
     int maxFixedPoints(vector<int>& nums) {
